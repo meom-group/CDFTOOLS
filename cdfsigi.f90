93274f1b204ca56dc980c1b6b1d8966b7d659e36
Jean-Marc Molines <Jean-Marc.Molines@univ-grenoble-alpes.fr>
2008-03-06 14:01:46 +0000

    JMM multi time capabilities in cdfsigi
    git-svn-id: https://servforge.legi.grenoble-inp.fr/svn/CDFTOOLS/CDFTOOLS/trunk@164 1055176f-818a-41d9-83e1-73fbe5b947c5

--- cdfsigi.f90
+++ cdfsigi.f90
@@ -22,16 +22,16 @@ PROGRAM cdfsigi
 
   !! * Local variables
   IMPLICIT NONE
-  INTEGER   :: jk                                  !: dummy loop index
+  INTEGER   :: jk , jt                             !: dummy loop index
   INTEGER   :: ierr                                !: working integer
   INTEGER   :: narg, iargc                         !: 
-  INTEGER   :: npiglo,npjglo, npk                  !: size of the domain
+  INTEGER   :: npiglo,npjglo, npk, npt             !: size of the domain
   INTEGER, DIMENSION(1) ::  ipk, &                 !: outptut variables : number of levels,
        &                    id_varout              !: ncdf varid's
   real(KIND=4) , DIMENSION (:,:), ALLOCATABLE :: ztemp, zsal ,&   !: Array to read a layer of data
        &                                         zsigi , &        !: potential density (sig-i)
        &                                         zmask            !: 2D mask at current level
-  REAL(KIND=4),DIMENSION(1)                   ::  tim
+  REAL(KIND=4),DIMENSION(:),ALLOCATABLE       :: tim
   REAL(KIND=4)                                :: prof=0.! in meters
   REAL(KIND=4)                                :: spval  !: missing value
 
@@ -59,6 +59,7 @@ PROGRAM cdfsigi
   npiglo= getdim (cfilet,'x')
   npjglo= getdim (cfilet,'y')
   npk   = getdim (cfilet,'depth')
+  npt   = getdim (cfilet,'time')
 
   ipk(:)= npk  ! all variables (input and output are 3D)
   typvar(1)%name= 'vosigmai'
@@ -75,8 +76,10 @@ PROGRAM cdfsigi
   PRINT *, 'npiglo=', npiglo
   PRINT *, 'npjglo=', npjglo
   PRINT *, 'npk   =', npk
+  PRINT *, 'npt   =', npt
 
   ALLOCATE (ztemp(npiglo,npjglo), zsal(npiglo,npjglo), zsigi(npiglo,npjglo) ,zmask(npiglo,npjglo))
+  ALLOCATE (tim(npt) )
 
   ! create output fileset
 
@@ -85,16 +88,17 @@ PROGRAM cdfsigi
   ierr= createvar(ncout ,typvar,1, ipk,id_varout )
   ierr= putheadervar(ncout, cfilet,npiglo, npjglo,npk)
 
-     spval=getatt(cfilet,'vosaline','missing_value')
-  DO jk = 1, npk
-     PRINT *,'level ',jk
-     zmask(:,:)=1.
-     IF (jk == 1  )  THEN
-        tim=getvar1d(cfilet,'time_counter',1)
-     END IF
+  spval=getatt(cfilet,'vosaline','missing_value')
 
-     ztemp(:,:)= getvar(cfilet, 'votemper',  jk ,npiglo, npjglo)
-     zsal(:,:) = getvar(cfilet, 'vosaline',  jk ,npiglo, npjglo)
+  tim=getvar1d(cfilet,'time_counter',npt)
+  ierr=putvar1d(ncout,tim,npt,'T')
+  DO jt = 1, npt
+     PRINT *,'time: ',jt
+  DO jk = 1, npk
+     zmask(:,:)=1.
+
+     ztemp(:,:)= getvar(cfilet, 'votemper',  jk ,npiglo, npjglo,ktime=jt)
+     zsal(:,:) = getvar(cfilet, 'vosaline',  jk ,npiglo, npjglo,ktime=jt)
 
      WHERE(zsal == spval ) zmask = 0
 
@@ -108,13 +112,10 @@ PROGRAM cdfsigi
        PRINT *,'               : max = ', sigmax,' at ', ismax(1), ismax(2)
      ENDIF
 
-     ierr = putvar(ncout, id_varout(1) ,zsigi, jk,npiglo, npjglo)
-
-     IF (jk == 1 )  THEN
-        ierr=putvar1d(ncout,tim,1,'T')
-     END IF
+     ierr = putvar(ncout, id_varout(1) ,zsigi, jk,npiglo, npjglo,ktime=jt)
 
   END DO  ! loop to next level
+  END DO  ! loop on time
 
   istatus = closeout(ncout)
 END PROGRAM cdfsigi
