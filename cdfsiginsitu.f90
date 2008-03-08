42ed38c3c21460b65a98fc46b97bfe33ddf653ed
Jean-Marc Molines <Jean-Marc.Molines@univ-grenoble-alpes.fr>
2008-03-08 10:04:28 +0000

    JMM alllows time frame in cdfsiginsitu + small bug fix on prof
    git-svn-id: https://servforge.legi.grenoble-inp.fr/svn/CDFTOOLS/CDFTOOLS/trunk@169 1055176f-818a-41d9-83e1-73fbe5b947c5

--- cdfsiginsitu.f90
+++ cdfsiginsitu.f90
@@ -22,17 +22,16 @@ PROGRAM cdfsiginsitu
 
   !! * Local variables
   IMPLICIT NONE
-  INTEGER   :: jk                                  !: dummy loop index
+  INTEGER   :: jk,jt                               !: dummy loop index
   INTEGER   :: ierr                                !: working integer
   INTEGER   :: narg, iargc                         !: 
-  INTEGER   :: npiglo,npjglo, npk                  !: size of the domain
+  INTEGER   :: npiglo,npjglo, npk ,npt             !: size of the domain
   INTEGER, DIMENSION(1) ::  ipk, &                 !: outptut variables : number of levels,
        &                    id_varout              !: ncdf varid's
   real(KIND=4) , DIMENSION (:,:), ALLOCATABLE :: ztemp, zsal ,&   !: Array to read a layer of data
        &                                         zsigi , &        !: potential density (sig-i)
        &                                         zmask            !: 2D mask at current level
-  REAL(KIND=4),DIMENSION(1)                   ::  tim
-  REAL(KIND=4),DIMENSION(:), ALLOCATABLE      :: prof             ! in meters
+  REAL(KIND=4),DIMENSION(:), ALLOCATABLE      :: prof ,tim    !: prof (m) and time (sec)
   REAL(KIND=4)                                :: spval  !: missing value
 
   CHARACTER(LEN=80) :: cfilet ,cfileout='siginsitu.nc' !:
@@ -55,11 +54,10 @@ PROGRAM cdfsiginsitu
   ENDIF
 
   CALL getarg (1, cfilet)
-  CALL getarg (2, cdum)
-  READ(cdum,*) prof
   npiglo= getdim (cfilet,'x')
   npjglo= getdim (cfilet,'y')
   npk   = getdim (cfilet,'depth')
+  npt   = getdim (cfilet,'time')
 
   ipk(:)= npk  ! all variables (input and output are 3D)
   typvar(1)%name= 'vosigmainsitu'
@@ -76,9 +74,10 @@ PROGRAM cdfsiginsitu
   PRINT *, 'npiglo=', npiglo
   PRINT *, 'npjglo=', npjglo
   PRINT *, 'npk   =', npk
+  PRINT *, 'npt   =', npt
 
   ALLOCATE (ztemp(npiglo,npjglo), zsal(npiglo,npjglo), zsigi(npiglo,npjglo) ,zmask(npiglo,npjglo))
-  ALLOCATE (prof(npk) )
+  ALLOCATE (prof(npk) , tim(npt) )
 
   ! create output fileset
 
@@ -86,16 +85,15 @@ PROGRAM cdfsiginsitu
 
   ierr= createvar(ncout ,typvar,1, ipk,id_varout )
   ierr= putheadervar(ncout, cfilet,npiglo, npjglo,npk)
-   prof(:)=getvar1d(cfilet,'deptht',npk)
+  prof(:)=getvar1d(cfilet,'deptht',npk)
+  tim=getvar1d(cfilet,'time_counter',npt)
+  ierr=putvar1d(ncout,tim,npt,'T')
 
-
-     spval=getatt(cfilet,'vosaline','missing_value')
+  spval=getatt(cfilet,'vosaline','missing_value')
+  DO jt=1,npt
+     PRINT *,'time ',jt, tim(jt)/86400.,' days'
   DO jk = 1, npk
-     PRINT *,'level ',jk
      zmask(:,:)=1.
-     IF (jk == 1  )  THEN
-        tim=getvar1d(cfilet,'time_counter',1)
-     END IF
 
      ztemp(:,:)= getvar(cfilet, 'votemper',  jk ,npiglo, npjglo)
      zsal(:,:) = getvar(cfilet, 'vosaline',  jk ,npiglo, npjglo)
@@ -103,22 +101,10 @@ PROGRAM cdfsiginsitu
      WHERE(zsal == spval ) zmask = 0
 
      zsigi(:,:) = sigmai ( ztemp,zsal,prof(jk),npiglo,npjglo )* zmask(:,:)
-     IF ( npiglo /= 1 .AND. npjglo /= 1 ) THEN
-       sigmin=minval(zsigi(2:npiglo-1,2:npjglo-1) ,zmask(2:npiglo-1,2:npjglo-1)==1)
-       sigmax=maxval(zsigi(2:npiglo-1,2:npjglo-1) ,zmask(2:npiglo-1,2:npjglo-1)==1)
-       ismin= minloc(zsigi(2:npiglo-1,2:npjglo-1) ,zmask(2:npiglo-1,2:npjglo-1)==1)
-       ismax= maxloc(zsigi(2:npiglo-1,2:npjglo-1) ,zmask(2:npiglo-1,2:npjglo-1)==1)
-       PRINT *,'Level ',jk,': min = ', sigmin,' at ', ismin(1), ismin(2)
-       PRINT *,'               : max = ', sigmax,' at ', ismax(1), ismax(2)
-     ENDIF
-
      ierr = putvar(ncout, id_varout(1) ,zsigi, jk,npiglo, npjglo)
 
-     IF (jk == 1 )  THEN
-        ierr=putvar1d(ncout,tim,1,'T')
-     END IF
-
   END DO  ! loop to next level
+  END DO  ! loop to next time
 
   istatus = closeout(ncout)
 END PROGRAM cdfsiginsitu
