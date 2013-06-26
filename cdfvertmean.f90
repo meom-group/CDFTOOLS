35e763a066443209553815d2ced94cff2a7f22fe
Jean-Marc Molines <Jean-Marc.Molines@univ-grenoble-alpes.fr>
2013-06-26 12:42:07 +0000

    JMM : add multi variable options in cdfvertmean tool
    git-svn-id: https://servforge.legi.grenoble-inp.fr/svn/CDFTOOLS/trunk@703 1055176f-818a-41d9-83e1-73fbe5b947c5

--- cdfvertmean.f90
+++ cdfvertmean.f90
@@ -10,6 +10,7 @@ PROGRAM cdfvertmean
   !!
   !! History : 2.1  : 11/2008  : J.M. Molines : Original code
   !!           3.0  : 01/2011  : J.M. Molines : Doctor norm + Lic.
+  !!           3.0  : 06/2013  : J.M. Molines : multi variables capability
   !!----------------------------------------------------------------------
   USE cdfio
   USE modcdfnames
@@ -22,15 +23,16 @@ PROGRAM cdfvertmean
   !!----------------------------------------------------------------------
   IMPLICIT NONE
 
-  INTEGER(KIND=4)                               :: jk, jvar, jt        ! dummy loop index
+  INTEGER(KIND=4)                               :: jk, jvar, jvarin, jt! dummy loop index
   INTEGER(KIND=4)                               :: ik1, ik2            ! vertical limit of integration
   INTEGER(KIND=4)                               :: narg, iargc         ! command line 
   INTEGER(KIND=4)                               :: ijarg, ireq         ! command line 
   INTEGER(KIND=4)                               :: npiglo, npjglo      ! size of the domain
   INTEGER(KIND=4)                               :: npk, npt            ! size of the domain,
   INTEGER(KIND=4)                               :: nvars, ivar         ! variables in input
+  INTEGER(KIND=4)                               :: nvaro=1             ! variables for output
   INTEGER(KIND=4)                               :: ncout, ierr         ! ncid and error status
-  INTEGER(KIND=4), DIMENSION(1)                 :: ipk, id_varout      ! levels and varid's of output vars
+  INTEGER(KIND=4), DIMENSION(:),    ALLOCATABLE :: ipk, id_varout      ! levels and varid's of output vars
 
   REAL(KIND=4)                                  :: rdep_up             ! upper level of integration
   REAL(KIND=4)                                  :: rdep_down           ! lower level of integration
@@ -49,8 +51,7 @@ PROGRAM cdfvertmean
 
   CHARACTER(LEN=256)                            :: cf_in               ! input file name
   CHARACTER(LEN=256)                            :: cf_out='vertmean.nc'! output file
-  CHARACTER(LEN=256)                            :: cv_in               ! variable name
-  CHARACTER(LEN=256)                            :: cv_out='sovertmean' ! variable name
+  CHARACTER(LEN=256)                            :: cv_cur              ! variable name
   CHARACTER(LEN=256)                            :: cv_dep              ! depth name
   CHARACTER(LEN=256)                            :: cv_e3               ! vertical metric name (partial)
   CHARACTER(LEN=256)                            :: cv_e31d             ! vertical metric name (full)
@@ -59,18 +60,20 @@ PROGRAM cdfvertmean
   CHARACTER(LEN=256)                            :: cglobal             ! global attribute
   CHARACTER(LEN=256)                            :: cldum               ! dummy string
   CHARACTER(LEN=256), DIMENSION(:), ALLOCATABLE :: cv_names            ! name of input variables
- 
-  TYPE(variable), DIMENSION(:), ALLOCATABLE     :: stypvarin           ! stucture for attributes (input)
-  TYPE(variable), DIMENSION(1)                  :: stypvar             ! stucture for attributes (output)
+  CHARACTER(LEN=256), DIMENSION(:), ALLOCATABLE :: cv_in               ! name of output variables
 
-  LOGICAL                                       :: lfull=.FALSE.       ! full step flag
+  TYPE(variable), DIMENSION(:), ALLOCATABLE     :: stypvarin           ! stucture for attributes (input)
+  TYPE(variable), DIMENSION(:), ALLOCATABLE     :: stypvar             ! stucture for attributes (output)
+
+  LOGICAL                                       :: lfull =.FALSE.      ! full step flag
+  LOGICAL                                       :: ldebug=.FALSE.      ! full step flag
   LOGICAL                                       :: lchk                ! file existence flag (true if missing)
   !!----------------------------------------------------------------------
   CALL ReadCdfNames()
 
   narg = iargc()
   IF ( narg == 0 ) THEN
-     PRINT *,' usage :  cdfvertmean IN-file IN-var v-type dep1 dep2 [-full]'
+     PRINT *,' usage :  cdfvertmean [-debug] IN-file IN-var1,var2,.. v-type dep1 dep2 [-full]'
      PRINT *,'      '
      PRINT *,'     PURPOSE :'
      PRINT *,'       Compute the vertical mean between dep1 and dep2 given in m,'
@@ -78,40 +81,43 @@ PROGRAM cdfvertmean
      PRINT *,'      '
      PRINT *,'     ARGUMENTS :'
      PRINT *,'       IN-file  : netcdf input file.' 
-     PRINT *,'       IN-var   : netcdf input variable.'
+     PRINT *,'       IN-var1,var2,.. : Comma separated list of input variables to process.'
      PRINT *,'       v-type   : one of T U V W indicating position of variable on C-grid'
-     PRINT *,'       dep1 dep2 : depths limit for vertical integration (meters). '
+     PRINT *,'       dep1 dep2 : depths limit for vertical integration (meters), from top '
+     PRINT *,'                to bottom, positive depths.'
      PRINT *,'      '
      PRINT *,'     OPTIONS :'
-     PRINT *,'       [-full ] : for full step configurations. Default is partial step.'
+     PRINT *,'       [-full  ] : for full step configurations. Default is partial step.'
+     PRINT *,'       [-debug ] : print some extra informations.'
      PRINT *,'      '
      PRINT *,'     REQUIRED FILES :'
      PRINT *,'       ', TRIM(cn_fzgr),' and ',TRIM(cn_fmsk)
      PRINT *,'      '
      PRINT *,'     OUTPUT : '
      PRINT *,'       netcdf file : ', TRIM(cf_out) 
-     PRINT *,'         variables : ', TRIM(cv_out),' (same units as input variable)'
+     PRINT *,'         variables : varin_vertmean (same units as input variable)'
      PRINT *,'      '
      STOP
   ENDIF
 
   ijarg = 1 ; ireq=0
   DO WHILE ( ijarg <= narg ) 
-    CALL getarg (ijarg, cldum ) ; ijarg = ijarg + 1
-    SELECT CASE ( cldum )
-    CASE ( '-full' ) ; lfull = .TRUE.
-    CASE DEFAULT
-       ireq=ireq+1
-       SELECT CASE ( ireq )
-       CASE ( 1 ) ; cf_in=cldum
-       CASE ( 2 ) ; cv_in=cldum
-       CASE ( 3 ) ; ctype=cldum
-       CASE ( 4 ) ; READ(cldum,*) rdep_up
-       CASE ( 5 ) ; READ(cldum,*) rdep_down
-       CASE DEFAULT
-          PRINT *,' Too many arguments ...' ; STOP
-       END SELECT
-    END SELECT
+     CALL getarg (ijarg, cldum ) ; ijarg = ijarg + 1
+     SELECT CASE ( cldum )
+     CASE ( '-full'  ) ; lfull  = .TRUE.
+     CASE ( '-debug' ) ; ldebug = .TRUE.
+     CASE DEFAULT
+        ireq=ireq+1
+        SELECT CASE ( ireq )
+        CASE ( 1 ) ; cf_in=cldum
+        CASE ( 2 ) ; CALL ParseVars(cldum)
+        CASE ( 3 ) ; ctype=cldum
+        CASE ( 4 ) ; READ(cldum,*) rdep_up
+        CASE ( 5 ) ; READ(cldum,*) rdep_down
+        CASE DEFAULT
+           PRINT *,' Too many arguments ...' ; STOP
+        END SELECT
+     END SELECT
   ENDDO
 
   lchk = chkfile (cn_fzgr)
@@ -131,50 +137,48 @@ PROGRAM cdfvertmean
   npk    = getdim (cf_in,cn_z)
   npt    = getdim (cf_in,cn_t)
 
-  nvars       = getnvar(cf_in)
-  ALLOCATE( cv_names(nvars), stypvarin(nvars) )
-  cv_names(:) = getvarname(cf_in, nvars, stypvarin)
-  ivar=1
-  DO jvar=1,nvars
-     IF ( TRIM(cv_names(jvar)) == TRIM(cv_in) ) THEN
-        EXIT
-     ENDIF
-     ivar=ivar+1
-  ENDDO
-
-  IF ( ivar == nvars+1 ) THEN
-     PRINT *,' Variable ',TRIM(cv_in),' not found in ', TRIM(cf_in)
-     STOP
-  ENDIF
-
-  rdep(1)                      = 0.
-  ipk(:)                       = 1
-  stypvar(1)%cname             = cv_out
-  stypvar(1)%cunits            = stypvarin(ivar)%cunits
-  stypvar(1)%rmissing_value    = stypvarin(ivar)%rmissing_value
-  stypvar(1)%valid_min         = stypvarin(ivar)%valid_min
-  stypvar(1)%valid_max         = stypvarin(ivar)%valid_max
-  stypvar(1)%clong_name        = 'vertical average of '//TRIM(stypvarin(ivar)%clong_name)
-  stypvar(1)%cshort_name       = cv_out
-  stypvar(1)%conline_operation = 'N/A'
-  stypvar(1)%caxis             = 'TYX'
-
   PRINT *, 'npiglo = ', npiglo
   PRINT *, 'npjglo = ', npjglo
   PRINT *, 'npk    = ', npk
   PRINT *, 'npt    = ', npt
+
+  nvars       = getnvar(cf_in)
+  ALLOCATE( cv_names(nvars), stypvarin(nvars) )
+  ALLOCATE( stypvar(nvaro), ipk(nvaro), id_varout(nvaro) )
+  cv_names(:) = getvarname(cf_in, nvars, stypvarin)
+
+  ! just chck if var exist in file 
+  DO jvar = 1, nvaro
+     IF ( chkvar( cf_in, cv_in(jvar)) ) STOP  ! message is written in cdfio.chkvar
+  ENDDO
 
   ! Allocate arrays
   ALLOCATE ( zmask(npiglo,npjglo), dvertmean(npiglo, npjglo) )
   ALLOCATE ( zv(npiglo,npjglo), hdep(npiglo,npjglo)          )
   ALLOCATE ( e3(npiglo,npjglo), dvol2d(npiglo,npjglo)        )
   ALLOCATE ( gdep(npk), tim(npt)                             )
-
   IF ( lfull ) ALLOCATE ( e31d(npk) )
+
+  rdep(1)                      = 0.
+  ipk(:)                       = 1
+  DO jvar=1, nvaro
+     DO jvarin=1,nvars
+        IF ( cv_in(jvar) == stypvarin(jvarin)%cname ) EXIT  ! cv_in match cv_varin.
+     END DO
+     stypvar(jvar)%cname             = TRIM(cv_in(jvar))//'_vert_mean'
+     stypvar(jvar)%cunits            = stypvarin(jvarin)%cunits
+     stypvar(jvar)%rmissing_value    = stypvarin(jvarin)%rmissing_value
+     stypvar(jvar)%valid_min         = stypvarin(jvarin)%valid_min
+     stypvar(jvar)%valid_max         = stypvarin(jvarin)%valid_max
+     stypvar(jvar)%clong_name        = 'vertical average of '//TRIM(stypvarin(jvarin)%clong_name)
+     stypvar(jvar)%cshort_name       = TRIM(cv_in(jvar))//'_vert_mean'
+     stypvar(jvar)%conline_operation = 'N/A'
+     stypvar(jvar)%caxis             = 'TYX'
+  END DO
 
   ! Initialize output file
   ncout = create      (cf_out, cf_in,   npiglo, npjglo, 1)
-  ierr  = createvar   (ncout,  stypvar, 1,      ipk,    id_varout, cdglobal=cglobal  )
+  ierr  = createvar   (ncout,  stypvar, nvaro,  ipk,    id_varout, cdglobal=cglobal  )
   ierr  = putheadervar(ncout,  cf_in,   npiglo, npjglo, 1, pdep=rdep)
 
   tim  = getvar1d(cf_in, cn_vtimec, npt     )
@@ -203,49 +207,91 @@ PROGRAM cdfvertmean
      IF ( gdep(jk) <= rdep_down ) ik2 = jk
   ENDDO
 
-  PRINT '(a,2f8.3)', 'depth limit of integration : ', rdep_up, rdep_down
-  PRINT '(a,2i8  )', 'nearest level found        : ', ik1,     ik2
-  PRINT '(a,2f8.3)', 'corresponding depth        : ', gdep(ik1), gdep(ik2+1)
+  PRINT '(a,2f10.3)', 'depth limit of integration : ', rdep_up, rdep_down
+  PRINT '(a,2i10  )', 'nearest level found        : ', ik1,     ik2
+  PRINT '(a,2f10.3)', 'corresponding depth        : ', gdep(ik1), gdep(MIN(ik2+1,npk))
 
   DO jt=1,npt
-     dvol           = 0.d0
-     dvol2d(:,:)    = 0.d0
-     dvertmean(:,:) = 0.d0
+     DO jvar = 1,nvaro
+        cv_cur = cv_in(jvar)
+        dvol           = 0.d0
+        dvol2d(:,:)    = 0.d0
+        dvertmean(:,:) = 0.d0
 
-     DO jk = ik1, ik2
-        ! Get values at jk
-        zv(   :,:) = getvar(cf_in,   cv_in,  jk, npiglo, npjglo, ktime=jt)
-        zmask(:,:) = getvar(cn_fmsk, cv_msk, jk, npiglo, npjglo          )
+        DO jk = ik1, ik2
+           ! Get values at jk
+           zv(   :,:) = getvar(cf_in,   cv_cur, jk, npiglo, npjglo, ktime=jt)
+           zmask(:,:) = getvar(cn_fmsk, cv_msk, jk, npiglo, npjglo          )
 
-        ! get e3 at level jk ( ps...)
-        IF ( lfull ) THEN ; e3(:,:) = e31d(jk)
-        ELSE              ; e3(:,:) = getvar(cn_fzgr, cv_e3, jk, npiglo, npjglo, ldiom=.TRUE.)
-        ENDIF
+           ! get e3 at level jk ( ps...)
+           IF ( lfull ) THEN ; e3(:,:) = e31d(jk)
+           ELSE              ; e3(:,:) = getvar(cn_fzgr, cv_e3, jk, npiglo, npjglo, ldiom=.TRUE.)
+           ENDIF
 
-        IF ( jk == ik1 ) THEN
-           hdep(:,:) = gdep(jk) + e3(:,:)
-           e3(  :,:) = MIN(e3, hdep -rdep_up          )
-        ENDIF
+           IF ( jk == ik1 ) THEN
+              hdep(:,:) = gdep(jk) + e3(:,:)
+              e3(  :,:) = MIN(e3, hdep -rdep_up          )
+           ENDIF
 
-        IF ( jk == ik2 ) THEN
-           e3(  :,:) = MIN(e3, (rdep_down) - gdep(jk) )
-        ENDIF
+           IF ( jk == ik2 ) THEN
+              e3(  :,:) = MIN(e3, (rdep_down) - gdep(jk) )
+           ENDIF
 
-        dvol       = SUM( DBLE(e3 * zmask) )
-        dvol2d     =      e3 * zmask * 1.d0  + dvol2d
-        dvertmean  = zv * e3 * zmask * 1.d0  + dvertmean
+           dvol       = SUM( DBLE(e3 * zmask) )
+           dvol2d     =      e3 * zmask * 1.d0  + dvol2d
+           dvertmean  = zv * e3 * zmask * 1.d0  + dvertmean
 
-        IF (dvol == 0 )THEN
-                  ! no more layer below !    
-           EXIT   ! get out of the jk loop
-        ENDIF
+           IF (dvol == 0 )THEN
+              ! no more layer below !    
+              EXIT   ! get out of the jk loop
+           ENDIF
+        END DO
+
+        ! Output to netcdf file 
+        WHERE ( dvol2d /= 0 )  dvertmean = dvertmean/dvol2d
+        ierr = putvar(ncout, id_varout(jvar), REAL(dvertmean), 1, npiglo, npjglo, ktime=jt)
      END DO
-
-     ! Output to netcdf file 
-     WHERE ( dvol2d /= 0 )  dvertmean = dvertmean/dvol2d
-     ierr = putvar(ncout, id_varout(1), REAL(dvertmean), 1, npiglo, npjglo, ktime=jt)
   END DO  ! loop on time
 
   ierr = closeout(ncout)
+CONTAINS
+  SUBROUTINE ParseVars (cdum)
+    !!---------------------------------------------------------------------
+    !!                  ***  ROUTINE ParseVars  ***
+    !!
+    !! ** Purpose :  Decode variable name  option from command line
+    !!
+    !! ** Method  :  look for , in the argument string and set the number of
+    !!         variable (nvaro), allocate cv_in array and fill it with the
+    !!         decoded  names.
+    !!
+    !!----------------------------------------------------------------------
+    CHARACTER(LEN=*), INTENT(in) :: cdum
+
+    CHARACTER(LEN=80), DIMENSION(100) :: cl_dum  ! 100 is arbitrary
+    INTEGER  :: ji
+    INTEGER  :: inchar,  i1=1
+    !!----------------------------------------------------------------------
+    inchar= LEN(TRIM(cdum))
+    ! scan the input string and look for ',' as separator
+    DO ji=1,inchar
+       IF ( cdum(ji:ji) == ',' ) THEN
+          cl_dum(nvaro) = cdum(i1:ji-1)
+          i1=ji+1
+          nvaro=nvaro+1
+       ENDIF
+    ENDDO
+
+    ! last name of the list does not have a ','
+    cl_dum(nvaro) = cdum(i1:inchar)
+
+    ALLOCATE ( cv_in(nvaro) )
+    IF ( ldebug) PRINT *,' SELECTED VARIABLES :'
+    DO ji=1, nvaro
+       cv_in(ji) = cl_dum(ji)
+       IF ( ldebug) PRINT *, "    ",TRIM(cv_in(ji))
+    ENDDO
+  END SUBROUTINE ParseVars
+
 
 END PROGRAM cdfvertmean
