15b642d9ba288bd8916754f6e7de7fa93d1e4713
Jean-Marc Molines <Jean-Marc.Molines@univ-grenoble-alpes.fr>
2015-10-09 19:09:52 +0000

    JMM : add options in cdfsig0 : -nc4 -o -sal -tem ( and -t )
    git-svn-id: https://servforge.legi.grenoble-inp.fr/svn/CDFTOOLS/trunk@860 1055176f-818a-41d9-83e1-73fbe5b947c5

--- cdfsig0.f90
+++ cdfsig0.f90
@@ -23,9 +23,9 @@ PROGRAM cdfsig0
 
   INTEGER(KIND=4)                           :: jk, jt             ! dummy loop index
   INTEGER(KIND=4)                           :: ierr               ! error status
-  INTEGER(KIND=4)                           :: narg, iargc        ! browse command line
+  INTEGER(KIND=4)                           :: narg, ijarg,iargc        ! browse command line
   INTEGER(KIND=4)                           :: npiglo, npjglo     ! size of the domain
-  INTEGER(KIND=4)                           :: npk, npt           ! size of the domain
+  INTEGER(KIND=4)                           :: npk, npkk, npt     ! size of the domain
   INTEGER(KIND=4)                           :: ncout              ! ncid of output file
   INTEGER(KIND=4), DIMENSION(1)             :: ipk, id_varout     ! level and  varid's
 
@@ -37,20 +37,32 @@ PROGRAM cdfsig0
 
   CHARACTER(LEN=256)                        :: cf_tfil            ! input filename
   CHARACTER(LEN=256)                        :: cf_out='sig0.nc'   ! output file name
+  CHARACTER(LEN=256)                        :: cldum              ! dummy characte variable variable
+  CHARACTER(LEN=256)                        :: cv_sal             ! salinity name in netcdf
+  CHARACTER(LEN=256)                        :: cv_tem             ! temperature name in netcdf
 
   TYPE (variable), DIMENSION(1)             :: stypvar            ! structure for attributes
+  LOGICAL                                   :: lnc4 = .FALSE.     ! flag for missing files
   !!----------------------------------------------------------------------
   CALL ReadCdfNames()
 
   narg = iargc()
   IF ( narg == 0 ) THEN
-     PRINT *,' usage : cdfsig0 T-file'
+     PRINT *,' usage : cdfsig0 -t T-file [-sal SAL-name] [-tem TEM-name] [-nc4] [-o OUT-file]'
      PRINT *,'      '
      PRINT *,'     PURPOSE :'
      PRINT *,'       Compute potential density (sigma-0) refered to the surface.' 
      PRINT *,'      '
      PRINT *,'     ARGUMENTS :'
-     PRINT *,'       T-file  : netcdf file with temperature and salinity.' 
+     PRINT *,'       -t T-file  : netcdf file with temperature and salinity.' 
+     PRINT *,'         (for backward compatibility, -t can be ommited when T-file is the only'
+     PRINT *,'         argument.)'
+     PRINT *,'      '
+     PRINT *,'     OPTIONS :'
+     PRINT *,'       [-sal SAL-name]  : name of salinity variable'
+     PRINT *,'       [-tem TEM-name]  : name of temperature variable'
+     PRINT *,'       [-nc4]  : enable chunking and compression'
+     PRINT *,'       [-o OUT-file]    : specify output filename instead of ',TRIM(cf_out)
      PRINT *,'      '
      PRINT *,'     REQUIRED FILES :'
      PRINT *,'        none'
@@ -64,7 +76,27 @@ PROGRAM cdfsig0
      STOP
   ENDIF
 
-  CALL getarg (1, cf_tfil)
+  ijarg=1
+  cv_sal=cn_vosaline
+  cv_tem=cn_votemper
+  DO WHILE ( ijarg <= narg )
+     CALL getarg(ijarg, cldum ) ; ijarg=ijarg+1
+     SELECT CASE ( cldum )
+     CASE ( '-t'   ) ; CALL getarg(ijarg, cf_tfil) ; ijarg=ijarg+1
+     CASE ( '-o'   ) ; CALL getarg(ijarg, cf_out ) ; ijarg=ijarg+1
+     CASE ( '-sal' ) ; CALL getarg(ijarg, cv_sal ) ; ijarg=ijarg+1
+     CASE ( '-tem' ) ; CALL getarg(ijarg, cv_tem ) ; ijarg=ijarg+1
+     CASE ( '-nc4' ) ; lnc4 = .TRUE.
+     CASE DEFAULT
+      IF ( narg == 1 ) THEN
+         cf_tfil = cldum
+      ELSE
+         PRINT *,' option ',TRIM(cldum),' not understood'
+         STOP
+      ENDIF
+     END SELECT
+  ENDDO
+ 
   IF (chkfile(cf_tfil) ) STOP ! missing file
 
   npiglo = getdim (cf_tfil, cn_x)
@@ -72,7 +104,10 @@ PROGRAM cdfsig0
   npk    = getdim (cf_tfil, cn_z)
   npt    = getdim (cf_tfil, cn_t)
 
-  ipk(:)                       = npk  ! all variables (input and output are 3D)
+  npkk=npk
+  IF ( npk == 0 ) npkk=1
+
+  ipk(:)                       = npkk  ! all variables (input and output are 3D)
   stypvar(1)%cname             = cn_vosigma0
   stypvar(1)%cunits            = 'kg/m3'
   stypvar(1)%rmissing_value    = 0.
@@ -82,6 +117,8 @@ PROGRAM cdfsig0
   stypvar(1)%cshort_name       = cn_vosigma0
   stypvar(1)%conline_operation = 'N/A'
   stypvar(1)%caxis             = 'TZYX'
+  stypvar(1)%ichunk            = (/npiglo, MAX(1,npjglo/30), 1, 1 /)
+
 
   PRINT *, 'npiglo = ', npiglo
   PRINT *, 'npjglo = ', npjglo
@@ -93,8 +130,8 @@ PROGRAM cdfsig0
   ALLOCATE (tim(npt) )
 
   ! create output fileset
-  ncout = create      (cf_out, cf_tfil, npiglo, npjglo, npk       )
-  ierr  = createvar   (ncout,  stypvar, 1,      ipk,    id_varout )
+  ncout = create      (cf_out, cf_tfil, npiglo, npjglo, npk,       ld_nc4=lnc4  )
+  ierr  = createvar   (ncout,  stypvar, 1,      ipk,    id_varout, ld_nc4=lnc4  )
   ierr  = putheadervar(ncout,  cf_tfil, npiglo, npjglo, npk       )
 
   tim=getvar1d(cf_tfil, cn_vtimec, npt     )
@@ -102,11 +139,11 @@ PROGRAM cdfsig0
 
   DO jt=1,npt
      PRINT *,' TIME = ', jt, tim(jt)/86400.,' days'
-     DO jk = 1, npk
+     DO jk = 1, npkk
         zmask(:,:)=1.
 
-        ztemp(:,:)= getvar(cf_tfil, cn_votemper, jk, npiglo, npjglo, ktime=jt)
-        zsal(:,:) = getvar(cf_tfil, cn_vosaline, jk, npiglo, npjglo, ktime=jt)
+        ztemp(:,:)= getvar(cf_tfil, cv_tem, jk, npiglo, npjglo, ktime=jt)
+        zsal(:,:) = getvar(cf_tfil, cv_sal, jk, npiglo, npjglo, ktime=jt)
 
         ! assuming spval is 0
         WHERE( zsal <= 0 ) zmask = 0
