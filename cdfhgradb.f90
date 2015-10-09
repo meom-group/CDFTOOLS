fb2b3f9c3799d2f3c95e80a1285a492157e60274
Jean-Marc Molines <Jean-Marc.Molines@univ-grenoble-alpes.fr>
2015-10-09 11:07:27 +0000

    JMM : add options to cdfhgradb     -nc4, -o , -sal and -tem     Allow the use with 2D files
    git-svn-id: https://servforge.legi.grenoble-inp.fr/svn/CDFTOOLS/trunk@858 1055176f-818a-41d9-83e1-73fbe5b947c5

--- cdfhgradb.f90
+++ cdfhgradb.f90
@@ -26,9 +26,9 @@ PROGRAM cdfhgradb
    INTEGER(KIND=4), PARAMETER                  :: jp_varout = 1     ! number of output variables
    INTEGER(KIND=4)                             :: jk, jt            ! dummy loop index
    INTEGER(KIND=4)                             :: ji, jj, jlev      ! dummy loop index
-   INTEGER(KIND=4)                             :: narg, iargc       ! command line
+   INTEGER(KIND=4)                             :: narg, ijarg, iargc! command line
    INTEGER(KIND=4)                             :: npiglo, npjglo    ! size of the domain
-   INTEGER(KIND=4)                             :: npk, npt          ! size of the domain
+   INTEGER(KIND=4)                             :: npk, npkk,  npt   ! size of the domain
    INTEGER(KIND=4)                             :: ncout             ! ncid of output variable
    INTEGER(KIND=4)                             :: ierr              ! error status
    INTEGER(KIND=4), DIMENSION(jp_varout)       :: ipk, id_varout    ! output variable
@@ -40,7 +40,7 @@ PROGRAM cdfhgradb
    REAL(KIND=4), DIMENSION(:),     ALLOCATABLE :: gdept             ! depth of Tlevels
    REAL(KIND=4)                                :: zdep              ! temporary scalar
 
-   REAL(KIND=8)                                :: zgrav=9.81
+   REAL(KIND=8)                                :: dgrav=9.81d0
 
    REAL(KIND=8), DIMENSION(:,:),   ALLOCATABLE :: dgradt_xu, dgradt_yv! Temperature gradient, native grid
    REAL(KIND=8), DIMENSION(:,:),   ALLOCATABLE :: dgrads_xu, dgrads_yv! Salinity gradient, native grid
@@ -56,48 +56,74 @@ PROGRAM cdfhgradb
    CHARACTER(LEN=256)                         :: cf_tfil             ! input file name for T and S
    CHARACTER(LEN=256)                         :: cf_sfil             ! input file name for S (optional)
    CHARACTER(LEN=256)                         :: cf_out = 'hgradb_gridT.nc' ! output file name
+   CHARACTER(LEN=256)                         :: cldum               ! dummy character variable
+   CHARACTER(LEN=256)                         :: cv_sal              ! salinity name in netcdf
+   CHARACTER(LEN=256)                         :: cv_tem              ! temperature name in netcdf
 
    TYPE(variable), DIMENSION(jp_varout)       :: stypvar             ! output data structure
 
    LOGICAL                                    :: lchk = .FALSE.      ! flag for missing files
+   LOGICAL                                    :: lnc4 = .FALSE.      ! flag for netcdf4 output with chunking and deflation
+
    !!----------------------------------------------------------------------
    CALL ReadCdfNames()
 
    narg= iargc()
    IF ( narg == 0 ) THEN
-      PRINT *,' usage : cdfhgradb T-file [S-file] '
+      PRINT *,' usage : cdfhgradb -t T-file [-s S-file] [-nc4 ] [-o OUT-file] ...'
+      PRINT *,'                   ...  [-sal SAL-name]  [-tem TEMP-name] '
       PRINT *,'      '
       PRINT *,'     PURPOSE :'
       PRINT *,'        Compute the norm of the horizontal buoyancy gradient.'
       PRINT *,'      Results are saved at T points.'
       PRINT *,'      '
       PRINT *,'     ARGUMENTS :'
-      PRINT *,'       T-file : File with ',TRIM(cn_votemper),' and ',TRIM(cn_vosaline),' variables' 
-      PRINT *,'           If ',TRIM(cn_vosaline),' not in T-file give a second name for S-file.'
+      PRINT *,'       -t T-file : File with ',TRIM(cn_votemper),' and ',TRIM(cn_vosaline),' variables' 
+      PRINT *,'           If ',TRIM(cn_vosaline),' not in T-file use -s option.'
+      PRINT *,'          Note that salinity and/or temperature variables name can be changed'
+      PRINT *,'          with -sal and/or -tem options, respectively.'
       PRINT *,'      '
       PRINT *,'     OPTIONS :'
-      PRINT *,'       S-file : File with ',TRIM(cn_vosaline),' variable if not in T file'
+      PRINT *,'      [-s S-file ] : File with ',TRIM(cn_vosaline),' variable if not in T file'
+      PRINT *,'      [-nc4      ] : use netcdf4 chunking and deflation on output '
+      PRINT *,'      [-o output file] : specify the name of output file instead of '
+      PRINT *,'                default name ',TRIM(cf_out)
+      PRINT *,'      [-sal SAL-name] : specify the name of salinity variable'
+      PRINT *,'      [-tem TEM-name] : specify the name of temperature variable'
       PRINT *,'      '
       PRINT *,'     REQUIRED FILES :'
       PRINT *,'       ', TRIM(cn_fhgr),' ',TRIM(cn_fmsk),' and ',TRIM(cn_fzgr)
       PRINT *,'      '
       PRINT *,'     OUTPUT : '
-      PRINT *,'       netcdf file : ', TRIM(cf_out) 
+      PRINT *,'       netcdf file : ', TRIM(cf_out),' ( unless specified with -o option)' 
       PRINT *,'         ',jp_varout,' variables : '
       PRINT *,'              vohgradb: norm of the horizontal buoyancy gradient at t-point'
       PRINT *,'      '
       PRINT *,'     SEE ALSO :'
-      PRINT *,'      '
+      PRINT *,'        cdfbuoyflx  '
       PRINT *,'      '
       STOP
    ENDIF
 
-   CALL getarg (1, cf_tfil)
-   IF ( narg == 2 ) THEN
-      CALL getarg( 2, cf_sfil)
-   ELSE
-      cf_sfil = cf_tfil   ! if only one file provided, they are identical
-   ENDIF
+   ijarg =1 
+   cv_sal=cn_vosaline
+   cv_tem=cn_votemper
+   DO WHILE ( ijarg <= narg )
+     CALL getarg (ijarg, cldum ) ; ijarg = ijarg + 1 
+     SELECT CASE ( cldum) 
+     CASE ( '-t '  ) ; CALL getarg (ijarg, cf_tfil ) ; ijarg = ijarg+1
+                          cf_sfil=cf_tfil
+     CASE ( '-s '  ) ; CALL getarg (ijarg, cf_sfil ) ; ijarg = ijarg+1
+     CASE ( '-nc4 ') ; lnc4 = .TRUE.
+     CASE ( '-o'   ) ; CALL getarg (ijarg, cf_out ) ; ijarg = ijarg+1
+     CASE ( '-sal' ) ; CALL getarg (ijarg, cv_sal ) ; ijarg = ijarg+1
+     CASE ( '-tem' ) ; CALL getarg (ijarg, cv_tem ) ; ijarg = ijarg+1
+     CASE DEFAULT
+       PRINT *, 'Option ',TRIM(cldum),' not understood !'
+       PRINT *, 'Check usage with cdfhgradb (no argument).'
+       STOP
+     END SELECT
+   ENDDO
 
    lchk = ( lchk .OR. chkfile(cf_tfil) )
    lchk = ( lchk .OR. chkfile(cf_sfil) )
@@ -116,9 +142,12 @@ PROGRAM cdfhgradb
    PRINT *, 'npk    = ', npk
    PRINT *, 'npt    = ', npt
 
+   npkk=npk
+   IF ( npk == 0 ) npkk = 1
+
    !!  Allocate arrays
    ALLOCATE (tim(npt) )
-   ALLOCATE (gdept(npk) )
+   ALLOCATE (gdept(npkk) )
    ALLOCATE (e1u  (npiglo,npjglo), e2v  (npiglo,npjglo) )
    ALLOCATE (umask(npiglo,npjglo), vmask(npiglo,npjglo), tmask(npiglo,npjglo))
    ALLOCATE (zt   (npiglo,npjglo), zs (npiglo,npjglo))
@@ -137,17 +166,17 @@ PROGRAM cdfhgradb
 
    e1u =  getvar(cn_fhgr, cn_ve1u, 1, npiglo, npjglo)
    e2v =  getvar(cn_fhgr, cn_ve2v, 1, npiglo, npjglo)
-   gdept(:)    = getvare3(cn_fzgr, cn_gdept, npk )
+   gdept(:)    = getvare3(cn_fzgr, cn_gdept, npkk )
 
    DO jt = 1,npt
-      DO jk = npk, 1, -1  
+      DO jk = 1, npkk
 
          PRINT *,'level ',jk
          zdep = gdept(jk)
 
          ! read files
-         zt(:,:) = getvar(cf_tfil, cn_votemper, jk,   npiglo, npjglo, ktime=jt)
-         zs(:,:) = getvar(cf_sfil, cn_vosaline, jk,   npiglo, npjglo, ktime=jt)
+         zt(:,:) = getvar(cf_tfil, cv_tem, jk,   npiglo, npjglo, ktime=jt)
+         zs(:,:) = getvar(cf_sfil, cv_sal, jk,   npiglo, npjglo, ktime=jt)
 
          umask(:,:) = getvar(cn_fmsk, 'umask' , jk, npiglo, npjglo )
          vmask(:,:) = getvar(cn_fmsk, 'vmask' , jk, npiglo, npjglo )
@@ -175,8 +204,8 @@ PROGRAM cdfhgradb
          dgrads_xt(:,:) = 0.d0
          dgrads_yt(:,:) = 0.d0
 
-         DO ji=npiglo,2,-1
-             DO jj=1,npjglo
+         DO ji=2,npiglo
+             DO jj=2,npjglo
                dgradt_xt(ji,jj) = 0.5*(dgradt_xu(ji-1,jj)+dgradt_xu(ji,jj))
                dgrads_xt(ji,jj) = 0.5*(dgrads_xu(ji-1,jj)+dgrads_xu(ji,jj))
                dgradt_yt(ji,jj) = 0.5*(dgradt_yv(ji,jj-1)+dgradt_yv(ji,jj))
@@ -200,7 +229,7 @@ PROGRAM cdfhgradb
          dgradb_yt(:,:) = zbeta(:,:) * ( zalbet(:,:) * dgradt_yt(:,:) - dgrads_yt(:,:) ) * tmask(:,:)
 
          ! get the norm of the buoyancy gradients at t-point
-         dgradb(:,:) = zgrav * SQRT( dgradb_xt(:,:) * dgradb_xt(:,:) + dgradb_yt(:,:) * dgradb_yt(:,:) )
+         dgradb(:,:) = dgrav * SQRT( dgradb_xt(:,:) * dgradb_xt(:,:) + dgradb_yt(:,:) * dgradb_yt(:,:) )
 
          ! write
          ierr = putvar(ncout, id_varout(1), REAL(dgradb), jk, npiglo, npjglo, ktime=jt)
@@ -220,7 +249,7 @@ CONTAINS
       !! ** Method  :  Use global module variables 
       !!
       !!----------------------------------------------------------------------
-      ipk(:) = npk  !  3D
+      ipk(:) = npkk  !  3D
 
 
       stypvar(1)%cname             = 'vohgradb'
@@ -231,10 +260,12 @@ CONTAINS
       stypvar(1)%clong_name        = 'Horizontal gradient of buoyancy.'
       stypvar(1)%cshort_name       = 'vohgradb'
       stypvar(1)%conline_operation = 'N/A'
+      stypvar(1)%ichunk            = (/npiglo, MAX(1,npjglo/30), 1, 1 /)
+
 
       ! create output fileset
-      ncout = create      (cf_out, cf_tfil, npiglo, npjglo, npk       )
-      ierr  = createvar   (ncout,  stypvar, jp_varout, ipk, id_varout )
+      ncout = create      (cf_out, cf_tfil, npiglo, npjglo, npk,       ld_nc4=lnc4    )
+      ierr  = createvar   (ncout,  stypvar, jp_varout, ipk, id_varout, ld_nc4=lnc4    )
       ierr  = putheadervar(ncout,  cf_tfil, npiglo, npjglo, npk       )
 
    END SUBROUTINE CreateOutput
