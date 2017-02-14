b4bc30bcb40d78601f665942e3b8f680d3d7bf6e
jmm <Jean-Marc.Molines@univ-grenoble-alpes.fr>
2017-02-14 21:27:30 +0100

    Add vvl option to cdfw
     * also add -o and -nc4 option
     * add CreateOutput

--- src/cdfw.f90
+++ src/cdfw.f90
@@ -23,9 +23,11 @@ PROGRAM cdfw
   IMPLICIT NONE
 
   INTEGER(KIND=4)                              :: ji, jj, jk, jt     ! dummy loop index
+  INTEGER(KIND=4)                              :: it                 ! time index for vvl
   INTEGER(KIND=4)                              :: npiglo, npjglo     ! size of the domain
   INTEGER(KIND=4)                              :: npk, npt           ! size of the domain
-  INTEGER(KIND=4)                              :: narg, iargc, ijarg ! browse line
+  INTEGER(KIND=4)                              :: narg, iargc        ! browse line
+  INTEGER(KIND=4)                              :: ijarg, ireq        ! browse line
   INTEGER(KIND=4)                              :: ncout              ! ncid of output file
   INTEGER(KIND=4)                              :: ierr               ! error status
   INTEGER(KIND=4)                              :: itop = 1           ! top array index
@@ -48,19 +50,22 @@ PROGRAM cdfw
 
   CHARACTER(LEN=256)                           :: cf_ufil            ! U file name
   CHARACTER(LEN=256)                           :: cf_vfil            ! V file name
+  CHARACTER(LEN=256)                           :: cf_tfil            ! Used for VVL e3t
   CHARACTER(LEN=256)                           :: cf_out='w.nc'      ! W file name ( output)
   CHARACTER(LEN=256)                           :: cldum              ! dummy string
 
   TYPE(variable), DIMENSION(1)                 :: stypvar            ! output attributes
 
   LOGICAL                                      :: lchk               ! missing files flag
-  LOGICAL                                      :: lfull=.FALSE.      ! full step flag
+  LOGICAL                                      :: lfull = .FALSE.    ! full step flag
+  LOGICAL                                      :: lnc4  = .FALSE.    ! Use nc4 with chunking and deflation
   !!----------------------------------------------------------------------
   CALL ReadCdfNames()
 
   narg = iargc()
   IF ( narg < 2 ) THEN
-     PRINT *,' usage : cdfw U-file V-file [ U-var V-var ] [ -full]'
+     PRINT *,' usage : cdfw U-file V-file [U-var V-var] [-full] [-o OUT-file] [-nc4] ...'
+     PRINT *,'           ...[-vvl T-file ]'
      PRINT *,'      '
      PRINT *,'     PURPOSE :'
      PRINT *,'       Compute the vertical velocity from the vertical integration of'
@@ -74,29 +79,42 @@ PROGRAM cdfw
      PRINT *,'       [ U-var V-var ] : names of the zonal and meridional velocity '
      PRINT *,'                         components. Default are ', TRIM(cn_vozocrtx),' and ', TRIM(cn_vomecrty)
      PRINT *,'       [ -full ] : in case of full step configuration. Default is partial step.'
+     PRINT *,'       [ -o OUT-file ] : specify the output file name instead of ', TRIM(cf_out)
+     PRINT *,'       [ -nc4 ]     : Use netcdf4 output with chunking and deflation level 1'
+     PRINT *,'                 This option is effective only if cdftools are compiled with'
+     PRINT *,'                 a netcdf library supporting chunking and deflation.'
+     PRINT *,'       [ -vvl T-file ] : Use time varying vertical metrics (e3t), provided '
+     PRINT *,'                in T-file'
      PRINT *,'      '
      PRINT *,'     REQUIRED FILES :'
      PRINT *,'       ',TRIM(cn_fhgr),' and ',TRIM(cn_fzgr) 
      PRINT *,'      '
      PRINT *,'     OUTPUT : '
-     PRINT *,'       netcdf file : ', TRIM(cf_out) 
+     PRINT *,'       netcdf file : ', TRIM(cf_out),' unless -o option is used.'
      PRINT *,'         variables : ', TRIM(cn_vovecrtz),' (m/s)'
      STOP
   ENDIF
 
-  ijarg = 1
-  CALL getarg(ijarg, cf_ufil) ; ijarg = ijarg + 1
-  CALL getarg(ijarg, cf_vfil) ; ijarg = ijarg + 1
-
+  ijarg = 1 ; ireq=0
   DO WHILE (ijarg <= narg )
-     CALL getarg(ijarg, cldum) 
+     CALL getarg(ijarg, cldum) ;  ijarg = ijarg + 1
      SELECT CASE ( cldum )
-     CASE ( '-full' ) 
-        lfull = .TRUE.
-        ijarg = ijarg + 1
+     CASE ( '-full' ) ; lfull  = .TRUE.
+     CASE ( '-vvl'  ) ; CALL getarg(ijarg, cf_tfil) ;  ijarg = ijarg + 1
+                        lg_vvl = .TRUE. 
+     CASE ( '-o'    ) ; CALL getarg(ijarg, cf_out ) ;  ijarg = ijarg + 1
+     CASE ( '-nc4'  ) ; lnc4   = .TRUE.
      CASE DEFAULT
-        CALL getarg(ijarg, cn_vozocrtx) ; ijarg = ijarg + 1
-        CALL getarg(ijarg, cn_vomecrty) ; ijarg = ijarg + 1
+        ireq=ireq+1
+        SELECT CASE ( ireq )
+        CASE ( 1 )  ; cf_ufil     = cldum
+        CASE ( 2 )  ; cf_vfil     = cldum
+        CASE ( 3 )  ; cn_vozocrtx = cldum
+        CASE ( 4 )  ; cn_vomecrty = cldum
+        CASE DEFAULT 
+           PRINT *, ' ERROR: Too many ''free'' arguments !'
+           STOP 1
+        END SELECT
      END SELECT
   END DO
 
@@ -106,22 +124,16 @@ PROGRAM cdfw
   lchk = chkfile (cf_vfil) .OR. lchk
   IF ( lchk ) STOP ! missing files
 
+  IF ( lg_vvl ) THEN 
+     cn_fe3u = cf_ufil
+     cn_fe3v = cf_vfil
+     cn_fe3t = cf_tfil
+  ENDIF
+
   npiglo = getdim(cf_ufil,cn_x)
   npjglo = getdim(cf_ufil,cn_y)
   npk    = getdim(cf_ufil,cn_z)
   npt    = getdim(cf_ufil,cn_t)
-
-  ! define new variables for output
-  ipk(1)                       = npk 
-  stypvar(1)%cname             = TRIM(cn_vovecrtz)
-  stypvar(1)%cunits            = 'm/s'
-  stypvar(1)%rmissing_value    = 0.
-  stypvar(1)%valid_min         = -1.
-  stypvar(1)%valid_max         = 1.
-  stypvar(1)%clong_name        = 'Vertical_Velocity'
-  stypvar(1)%cshort_name       = TRIM(cn_vovecrtz)
-  stypvar(1)%conline_operation = 'N/A'
-  stypvar(1)%caxis             = 'TZYX'
 
   ! Allocate the memory
   ALLOCATE ( e1v(npiglo,npjglo), e2u(npiglo,npjglo) )
@@ -147,15 +159,12 @@ PROGRAM cdfw
   gdepw(:) = getvare3(cn_fzgr, cn_gdepw, npk)
   IF ( lfull ) e31d(:) = getvare3(cn_fzgr, cn_ve3t, npk)
 
-  ! create output fileset
-  ncout = create      (cf_out, cf_ufil, npiglo, npjglo, npk, cdep=cn_vdepthw     )
-  ierr  = createvar   (ncout,  stypvar, 1,      ipk,    id_varout                )
-  ierr  = putheadervar(ncout,  'dummy', npiglo, npjglo, npk, glamt, gphit, gdepw )
-
-  tim  = getvar1d(cf_ufil, cn_vtimec, npt     )
-  ierr = putvar1d(ncout, tim,       npt, 'T')
+  CALL CreateOutput
 
   DO jt = 1, npt
+     IF ( lg_vvl ) THEN ;  it=jt
+     ELSE               ;  it=1
+     ENDIF
      wn(:,:,:) = 0.
      ! Main level loop from bottom to top
      DO jk = npk-1, 1, -1
@@ -171,9 +180,9 @@ PROGRAM cdfw
            e3t(:,:) = e31d(jk)
         ELSE
            ! e3 metrics at level jk ( Partial steps)
-           e3u(:,:) = getvar(cn_fzgr, 'e3u_ps', jk, npiglo, npjglo, ldiom=.TRUE.) 
-           e3v(:,:) = getvar(cn_fzgr, 'e3v_ps', jk, npiglo, npjglo, ldiom=.TRUE.) 
-           e3t(:,:) = getvar(cn_fzgr, 'e3t_ps', jk, npiglo, npjglo, ldiom=.TRUE.) 
+           e3u(:,:) = getvar(cn_fe3u, cn_ve3u, jk, npiglo, npjglo, ktime=it, ldiom=.NOT.lg_vvl ) 
+           e3v(:,:) = getvar(cn_fe3v, cn_ve3v, jk, npiglo, npjglo, ktime=it, ldiom=.NOT.lg_vvl ) 
+           e3t(:,:) = getvar(cn_fe3t, cn_ve3t, jk, npiglo, npjglo, ktime=it, ldiom=.NOT.lg_vvl ) 
         ENDIF
 
         ! Compute divergence :
@@ -200,6 +209,38 @@ PROGRAM cdfw
 
   ierr = closeout(ncout)
 
+CONTAINS
+  SUBROUTINE CreateOutput
+    !!---------------------------------------------------------------------
+    !!                  ***  ROUTINE CreateOutput  ***
+    !!
+    !! ** Purpose :  Create netcdf output file(s) 
+    !!
+    !! ** Method  :  Use stypvar global description of variables
+    !!
+    !!----------------------------------------------------------------------
+    ! define new variables for output
+    ipk(1)                       = npk 
+    stypvar(1)%ichunk            = (/npiglo,MAX(1,npjglo/30),1,1 /)
+    stypvar(1)%cname             = TRIM(cn_vovecrtz)
+    stypvar(1)%cunits            = 'm/s'
+    stypvar(1)%rmissing_value    = 0.
+    stypvar(1)%valid_min         = -1.
+    stypvar(1)%valid_max         = 1.
+    stypvar(1)%clong_name        = 'Vertical_Velocity'
+    stypvar(1)%cshort_name       = TRIM(cn_vovecrtz)
+    stypvar(1)%conline_operation = 'N/A'
+    stypvar(1)%caxis             = 'TZYX'
+
+    ! create output fileset
+    ncout = create      (cf_out, cf_ufil,  npiglo, npjglo, npk, cdep=cn_vdepthw  , ld_nc4=lnc4 )
+    ierr  = createvar   (ncout , stypvar,  1,      ipk,    id_varout,              ld_nc4=lnc4 )
+    ierr  = putheadervar(ncout,  'dummy',  npiglo, npjglo, npk, glamt, gphit, gdepw            )
+
+    tim  = getvar1d(cf_ufil, cn_vtimec, npt     )
+    ierr = putvar1d(ncout, tim,       npt, 'T')
+
+  END SUBROUTINE CreateOutput
 
 END PROGRAM cdfw
 
