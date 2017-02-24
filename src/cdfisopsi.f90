1629cfe1271aab87ddb2244a00a2f7eeab0741b6
jmm <Jean-Marc.Molines@univ-grenoble-alpes.fr>
2017-02-24 12:34:34 +0100

    add vvl option in cdfisopsi
      * also add -o and -nc4 option
      * introduce CreateOutput subroutine
      * Clean the code using a routine ProjectOverIso instead of repeating code lines.

--- src/cdfisopsi.f90
+++ src/cdfisopsi.f90
@@ -25,9 +25,10 @@ PROGRAM cdfisopsi
   IMPLICIT NONE
   INTEGER(KIND=4), PARAMETER  :: jp_vars=7
 
-  INTEGER(KIND=4)                             :: jj, ji, jk, jt            ! dummy loop index
+  INTEGER(KIND=4)                             :: jj, ji, jk, jt ,jv        ! dummy loop index
+  INTEGER(KIND=4)                             :: it                        ! time index for vvl
   INTEGER(KIND=4)                             :: ierr                      ! working integer
-  INTEGER(KIND=4)                             :: narg, iargc               ! command line arguments
+  INTEGER(KIND=4)                             :: narg, iargc, ijarg        ! command line arguments
   INTEGER(KIND=4)                             :: npiglo, npjglo  ! size of the domain
   INTEGER(KIND=4)                             :: npk, npt  ! size of the domain
   INTEGER(KIND=4)                             :: ik, ik0                        ! 
@@ -64,32 +65,66 @@ PROGRAM cdfisopsi
 
   CHARACTER(LEN=256) :: cf_tfil                              ! input gridT file
   CHARACTER(LEN=256) :: cf_out='isopsi.nc'                   ! output file name
+  CHARACTER(LEN=256) :: cv_out='soisopsi'                    ! output variable name
   CHARACTER(LEN=256) :: cldum                                ! dummy character variable for reading
+
+  LOGICAL            :: lnc4 = .FALSE.                       ! flag for netcdf4 output
 
   TYPE(variable) , DIMENSION(jp_vars) :: stypvar         ! structure for attributes
   !!----------------------------------------------------------------------
   CALL ReadCdfNames()
 
   narg= iargc()
+
   IF ( narg == 0 ) THEN
-     PRINT *,' usage : cdfisopsi ref_level sigma_ref gridT '
-     PRINT *,'      Compute  a geostrophic streamfunction'
-     PRINT *,'      projected  on an isopycn.'
-     PRINT *,'         ref_level = reference level for pot. density'
-     PRINT *,'         sigma_ref = density level to project on'
-     PRINT *,'         gridT     = input file for temperature and salinity'
-     PRINT *,'  '
-     PRINT *,'         Output on ',TRIM(cf_out),' variable soisopsi'
-     PRINT *,'         Depths are taken from input file '
-     PRINT *,'         requires ',TRIM(cn_fhgr),' and ',TRIM(cn_fzgr) 
+     PRINT *,' usage :  cdfisopsi -ref REF-level -sig TGT-sigma -f T-file [-o OUT-file]...'
+     PRINT *,'          ... [-nc4] [-vvl] '
+     PRINT *,'      '
+     PRINT *,'     PURPOSE :'
+     PRINT *,'       Compute  a geostrophic streamfunction, projected  on an isopycn.'
+     PRINT *,'      '
+     PRINT *,'     ARGUMENTS :'
+     PRINT *,'        -ref REF-level: reference level for pot. density.'
+     PRINT *,'        -sig TGT-sigma: target density level to project on.'
+     PRINT *,'        -f T-file : input file for temperature and salinity.'
+     PRINT *,'      '
+     PRINT *,'     OPTIONS :'
+     PRINT *,'        [-o OUT-file ]: specify output filename instead of ',TRIM(cf_out)
+     PRINT *,'        [ -nc4 ]     : Use netcdf4 output with chunking and deflation level 1'
+     PRINT *,'                 This option is effective only if cdftools are compiled with'
+     PRINT *,'                 a netcdf library supporting chunking and deflation.'
+     PRINT *,'        [ -vvl ] : use time varying vertical metrics.'
+     PRINT *,'      '
+     PRINT *,'     REQUIRED FILES :'
+     PRINT *,'       ',TRIM(cn_fhgr),' and ', TRIM(cn_fzgr) 
+     PRINT *,'      '
+     PRINT *,'     OUTPUT : '
+     PRINT *,'       netcdf file : ', TRIM(cf_out),' unless -o option is used.'
+     PRINT *,'         variables : ', TRIM(cv_out),' (    )'
+     PRINT *,'      '
+     PRINT *,'     SEE ALSO :'
+     PRINT *,'      '
+     PRINT *,'      '
      STOP
   ENDIF
 
-  CALL getarg (1, cldum) ; READ (cldum,*) refdepth
-  CALL getarg (2, cldum) ; READ (cldum,*) zsigmaref
-  CALL getarg (3, cf_tfil)
+  ijarg = 1
+  DO WHILE ( ijarg <= narg ) 
+     CALL getarg(ijarg, cldum) ; ijarg = ijarg+1
+     SELECT CASE ( cldum )
+     CASE ( '-ref' ) ; CALL getarg(ijarg, cldum  ) ; ijarg = ijarg+1 ; READ(cldum,*) refdepth
+     CASE ( '-sig' ) ; CALL getarg(ijarg, cldum  ) ; ijarg = ijarg+1 ; READ(cldum,*) zsigmaref
+     CASE ( '-f'   ) ; CALL getarg(ijarg, cf_tfil) ; ijarg = ijarg+1 
+     ! options
+     CASE ( '-o'   ) ; CALL getarg(ijarg, cf_out ) ; ijarg = ijarg+1 
+     CASE ( '-nc4' ) ; lnc4   = .TRUE.
+     CASE ( '-vvl' ) ; lg_vvl = .TRUE.
+     END SELECT
+  ENDDO
 
   IF ( chkfile(cf_tfil) .OR. chkfile(cn_fzgr) .OR. chkfile(cn_fhgr) ) STOP  ! missing file
+
+  IF ( lg_vvl ) cn_fe3t = cf_tfil
 
   PRINT *, 'Potential density referenced at ', refdepth , ' meters'
   PRINT *, 'Isopycn for projection is ', zsigmaref
@@ -111,92 +146,14 @@ PROGRAM cdfisopsi
   e2t(:,:) = getvar(cn_fhgr, cn_ve2t, 1, npiglo, npjglo)
 
   !--------------------------------------------------------------------
-  !!  Output file
-  ipk(:)= 1  ! all variables are 2d
-  stypvar(1)%cname             = 'votemper_interp'
-  stypvar(1)%cunits            = 'DegC'
-  stypvar(1)%rmissing_value    = 0.
-  stypvar(1)%valid_min         = -2.
-  stypvar(1)%valid_max         = 45.
-  stypvar(1)%clong_name        = 'Temperature interpolated on isopycnal layer'
-  stypvar(1)%cshort_name       = 'votemper_interp'
-  stypvar(1)%conline_operation = 'N/A'
-  stypvar(1)%caxis             = 'TZYX'
-
-  stypvar(2)%cname             = 'vosaline_interp'
-  stypvar(2)%cunits            = 'PSU'
-  stypvar(2)%rmissing_value    = 0.
-  stypvar(2)%valid_min         = 0.
-  stypvar(2)%valid_max         = 50.
-  stypvar(2)%clong_name        = 'Salinity interpolated on isopycnal layer'
-  stypvar(2)%cshort_name       = 'vosaline_interp'
-  stypvar(2)%conline_operation = 'N/A'
-  stypvar(2)%caxis             = 'TZYX'
-
-  stypvar(3)%cname             = 'depth_interp'
-  stypvar(3)%cunits            = 'meters'
-  stypvar(3)%rmissing_value    = 0.
-  stypvar(3)%valid_min         = 0.0
-  stypvar(3)%valid_max         = 8000.
-  stypvar(3)%clong_name        = 'Depth of the isopycnal layer'
-  stypvar(3)%cshort_name       = 'depth_interp'
-  stypvar(3)%conline_operation = 'N/A'
-  stypvar(3)%caxis             = 'TZYX'
-
-  stypvar(4)%cname             = 'soisopsi'
-  stypvar(4)%cunits            = 'm2s-2 (to be verified)'
-  stypvar(4)%rmissing_value    = 0.
-  stypvar(4)%valid_min         = -500.
-  stypvar(4)%valid_max         =  500.
-  stypvar(4)%clong_name        = 'Total streamfunction on the isopycnal layer'
-  stypvar(4)%cshort_name       = 'soisopsi'
-  stypvar(4)%conline_operation = 'N/A'
-  stypvar(4)%caxis             = 'TZYX'
-
-  stypvar(5)%cname             = 'soisopsi0'
-  stypvar(5)%cunits            = 'm2s-2 (to be verified)'
-  stypvar(5)%rmissing_value    = 0.
-  stypvar(5)%valid_min         = -500.
-  stypvar(5)%valid_max         =  500.
-  stypvar(5)%clong_name        = 'Contribution of the SSH'
-  stypvar(5)%cshort_name       = 'soisopsi'
-  stypvar(5)%conline_operation = 'N/A'
-  stypvar(5)%caxis             = 'TZYX'
-
-  stypvar(6)%cname             = 'soisopsi1'
-  stypvar(6)%cunits            = 'm2s-2 (to be verified)'
-  stypvar(6)%rmissing_value    = 0.
-  stypvar(6)%valid_min         = -500.
-  stypvar(6)%valid_max         =  500.
-  stypvar(6)%clong_name        = 'Contribution of specific volume anomaly vertical integration'
-  stypvar(6)%cshort_name       = 'soisopsi'
-  stypvar(6)%conline_operation = 'N/A'
-  stypvar(6)%caxis             = 'TZYX'
-
-  stypvar(7)%cname             = 'soisopsi2'
-  stypvar(7)%cunits            = 'm2s-2 (to be verified)'
-  stypvar(7)%rmissing_value    = 0.
-  stypvar(7)%valid_min         = -500.
-  stypvar(7)%valid_max         =  500.
-  stypvar(7)%clong_name        = 'Contribution of pressure term on the isopycnal layer'
-  stypvar(7)%cshort_name       = 'soisopsi'
-  stypvar(7)%conline_operation = 'N/A'
-  stypvar(7)%caxis             = 'TZYX'
-
-  ! create output fileset
-  ncout = create      (cf_out, cf_tfil, npiglo,  npjglo, npk       )
-  ierr  = createvar   (ncout,  stypvar, jp_vars, ipk,    id_varout ) 
-  ierr  = putheadervar(ncout,  cf_tfil, npiglo,  npjglo, npk       )
-
-  prof(:) = getvar1d(cf_tfil, cn_vdeptht, npk     )
-  tim     = getvar1d(cf_tfil, cn_vtimec,  npt     )
-  ierr    = putvar1d(ncout,   tim,        npt, 'T')
-
+  CALL CreateOutput
   zspval   = getatt(cf_tfil, cn_vosaline, cn_missing_value )
-
   !---------------------------------------------------------------------------
   DO jt=1,npt
      PRINT *,'time ',jt, tim(jt)/86400.,' days'
+     IF (lg_vvl ) THEN ; it = jt
+     ELSE              ; it = 1
+     ENDIF
 
      !------------------------------------------------------------------------------
      ! 1. First we compute the potential density and store it into a 3d array
@@ -219,7 +176,7 @@ PROGRAM cdfisopsi
      ! 2. Projection of T,S and p on the chosen isopycnal layer (from cdfrhoproj)
      ALLOCATE ( alpha(npiglo,npjglo) )
 
-     !! Compute coefficients
+     !! Compute  the interpolation coefficients
      DO ji=1,npiglo
         DO jj = 1, npjglo
            ik = 1
@@ -253,30 +210,7 @@ PROGRAM cdfisopsi
         ztemp3(:,:,jk) = getvar(cf_tfil, cn_votemper, jk ,npiglo, npjglo, ktime=jt)
      ENDDO
 
-     DO ji=1,npiglo
-        DO jj=1,npjglo
-           ! ik0 is retrieved from alpha, taking the integer part.
-           ! The remnant is alpha. 
-           ik0=INT(alpha(ji,jj))
-           alpha(ji,jj) =  alpha(ji,jj) - ik0
-           IF (ik0 /= 0) THEN
-              P1=ztemp3(ji,jj,ik0)
-              P2=ztemp3(ji,jj,ik0+1)
-              IF (P1 /= zspval .AND. P2 /= zspval) THEN
-                 ztempint(ji,jj) = alpha(ji,jj) * P2          + (1-alpha(ji,jj)) * P1
-                 zint(ji,jj)     = alpha(ji,jj) * prof(ik0+1) + (1-alpha(ji,jj)) * prof(ik0)
-              ELSE
-                 ztempint(ji,jj) = zspval
-                 zint(ji,jj)     = zspval
-              ENDIF
-           ELSE
-              ztempint(ji,jj) = zspval
-              zint(ji,jj)     = zspval
-           ENDIF
-           ! re-add ik0 to alpha for the next computation
-           alpha(ji,jj) =  alpha(ji,jj) + ik0
-        END DO
-     END DO
+     CALL ProjectOverIso ( ztemp3, ztempint, prof, zint )
 
      pint = zint / 10. ! pressure on the isopycnal layer = depth / 10.
 
@@ -291,28 +225,8 @@ PROGRAM cdfisopsi
      DO jk=1,npk
         zsal3(:,:,jk) = getvar(cf_tfil, cn_vosaline, jk, npiglo, npjglo, ktime=jt)
      ENDDO
-
-     DO ji=1,npiglo
-        DO jj=1,npjglo
-           ! ik0 is retrieved from alpha, taking the integer part.
-           ! The remnant is alpha. 
-           ik0=INT(alpha(ji,jj))
-           alpha(ji,jj) =  alpha(ji,jj) - ik0
-           IF (ik0 /= 0) THEN
-              P1=zsal3(ji,jj,ik0)
-              P2=zsal3(ji,jj,ik0+1)
-              IF (P1 /= zspval .AND. P2 /= zspval) THEN
-                 zsalint(ji,jj) = alpha(ji,jj) * P2 + (1-alpha(ji,jj)) * P1
-              ELSE
-                 zsalint(ji,jj) = zspval
-              ENDIF
-           ELSE
-              zsalint(ji,jj) = zspval
-           ENDIF
-           ! re-add ik0 to alpha for the next computation
-           alpha(ji,jj) =  alpha(ji,jj) + ik0
-        END DO
-     END DO
+     
+     CALL ProjectOverIso (zsal3, zsalint)
 
      ierr = putvar(ncout, id_varout(2), zsalint, 1, npiglo, npjglo, ktime=jt)
      DEALLOCATE( zsal3 )
@@ -360,7 +274,7 @@ PROGRAM cdfisopsi
 
      rdeltapsi1(:,:) = 0.
      DO jk=1, npk
-        zdep(:,:) = getvar(cn_fzgr, 'e3t_ps', jk,npiglo,npjglo,ldiom=.TRUE.)
+        zdep(:,:) = getvar(cn_fe3t, cn_ve3t, jk, npiglo, npjglo, ktime=it, ldiom=.NOT.lg_vvl )
 
         ! For each point we integrate from surface to zint(ji,jj) which is the depth
         ! of the isopycnal layer
@@ -377,27 +291,7 @@ PROGRAM cdfisopsi
      ! 6. Projection of the specific volume anomaly on the isopycnal layer
      ALLOCATE( zsva2(npiglo,npjglo), rdeltapsi2(npiglo,npjglo) )
 
-     DO ji=1,npiglo
-        DO jj=1,npjglo
-           ! ik0 is retrieved from alpha, taking the integer part.
-           ! The remnant is alpha. 
-           ik0=INT(alpha(ji,jj))
-           alpha(ji,jj) =  alpha(ji,jj) - ik0
-           IF (ik0 /= 0) THEN
-              P1=zsva3(ji,jj,ik0)
-              P2=zsva3(ji,jj,ik0+1)
-              IF (P1 /= zspval .AND. P2 /= zspval) THEN
-                 zsva2(ji,jj) = alpha(ji,jj) * P2 + ( 1-alpha(ji,jj) ) * P1
-              ELSE
-                 zsva2(ji,jj) = zspval
-              ENDIF
-           ELSE
-              zsva2(ji,jj) = zspval
-           ENDIF
-           ! re-add ik0 to alpha for the next computation
-           alpha(ji,jj) =  alpha(ji,jj) + ik0
-        END DO
-     END DO
+     CALL ProjectOverIso ( zsva3, zsva2 )
 
      rdeltapsi2 = ( pint - pmean ) * zsva2
      ierr       = putvar(ncout, id_varout(7), rdeltapsi2, 1, npiglo, npjglo, ktime=jt)
@@ -432,5 +326,158 @@ PROGRAM cdfisopsi
   END DO  ! loop to next time
 
   ierr = closeout(ncout)
+
+CONTAINS
+
+  SUBROUTINE CreateOutput
+    !!---------------------------------------------------------------------
+    !!                  ***  ROUTINE CreateOutput  ***
+    !!
+    !! ** Purpose :  Create netcdf output file(s) 
+    !!
+    !! ** Method  :  Use stypvar global description of variables
+    !!
+    !!----------------------------------------------------------------------
+    DO jv = 1, jp_vars
+      stypvar(jv)%ichunk            = (/npiglo,MAX(1,npjglo/30),1,1 /)
+    ENDDO
+
+    ipk(:)= 1  ! all variables are 2d
+    stypvar(1)%cname             = 'votemper_interp'
+    stypvar(1)%cunits            = 'DegC'
+    stypvar(1)%rmissing_value    = 0.
+    stypvar(1)%valid_min         = -2.
+    stypvar(1)%valid_max         = 45.
+    stypvar(1)%clong_name        = 'Temperature interpolated on isopycnal layer'
+    stypvar(1)%cshort_name       = 'votemper_interp'
+    stypvar(1)%conline_operation = 'N/A'
+    stypvar(1)%caxis             = 'TZYX'
+  
+    stypvar(2)%cname             = 'vosaline_interp'
+    stypvar(2)%cunits            = 'PSU'
+    stypvar(2)%rmissing_value    = 0.
+    stypvar(2)%valid_min         = 0.
+    stypvar(2)%valid_max         = 50.
+    stypvar(2)%clong_name        = 'Salinity interpolated on isopycnal layer'
+    stypvar(2)%cshort_name       = 'vosaline_interp'
+    stypvar(2)%conline_operation = 'N/A'
+    stypvar(2)%caxis             = 'TZYX'
+
+    stypvar(3)%cname             = 'depth_interp'
+    stypvar(3)%cunits            = 'meters'
+    stypvar(3)%rmissing_value    = 0.
+    stypvar(3)%valid_min         = 0.0
+    stypvar(3)%valid_max         = 8000.
+    stypvar(3)%clong_name        = 'Depth of the isopycnal layer'
+    stypvar(3)%cshort_name       = 'depth_interp'
+    stypvar(3)%conline_operation = 'N/A'
+    stypvar(3)%caxis             = 'TZYX'
+
+    stypvar(4)%cname             = 'soisopsi'
+    stypvar(4)%cunits            = 'm2s-2 (to be verified)'
+    stypvar(4)%rmissing_value    = 0.
+    stypvar(4)%valid_min         = -500.
+    stypvar(4)%valid_max         =  500.
+    stypvar(4)%clong_name        = 'Total streamfunction on the isopycnal layer'
+    stypvar(4)%cshort_name       = 'soisopsi'
+    stypvar(4)%conline_operation = 'N/A'
+    stypvar(4)%caxis             = 'TZYX'
+
+    stypvar(5)%cname             = 'soisopsi0'
+    stypvar(5)%cunits            = 'm2s-2 (to be verified)'
+    stypvar(5)%rmissing_value    = 0.
+    stypvar(5)%valid_min         = -500.
+    stypvar(5)%valid_max         =  500.
+    stypvar(5)%clong_name        = 'Contribution of the SSH'
+    stypvar(5)%cshort_name       = 'soisopsi'
+    stypvar(5)%conline_operation = 'N/A'
+    stypvar(5)%caxis             = 'TZYX'
+
+    stypvar(6)%cname             = 'soisopsi1'
+    stypvar(6)%cunits            = 'm2s-2 (to be verified)'
+    stypvar(6)%rmissing_value    = 0.
+    stypvar(6)%valid_min         = -500.
+    stypvar(6)%valid_max         =  500.
+    stypvar(6)%clong_name        = 'Contribution of specific volume anomaly vertical integration'
+    stypvar(6)%cshort_name       = 'soisopsi'
+    stypvar(6)%conline_operation = 'N/A'
+    stypvar(6)%caxis             = 'TZYX'
+
+    stypvar(7)%cname             = 'soisopsi2'
+    stypvar(7)%cunits            = 'm2s-2 (to be verified)'
+    stypvar(7)%rmissing_value    = 0.
+    stypvar(7)%valid_min         = -500.
+    stypvar(7)%valid_max         =  500.
+    stypvar(7)%clong_name        = 'Contribution of pressure term on the isopycnal layer'
+    stypvar(7)%cshort_name       = 'soisopsi'
+    stypvar(7)%conline_operation = 'N/A'
+    stypvar(7)%caxis             = 'TZYX'
+
+    ! create output fileset
+    ncout = create      (cf_out, cf_tfil, npiglo,  npjglo, npk       , ld_nc4=lnc4 )
+    ierr  = createvar   (ncout,  stypvar, jp_vars, ipk,    id_varout , ld_nc4=lnc4 ) 
+    ierr  = putheadervar(ncout,  cf_tfil, npiglo,  npjglo, npk                     )
+
+    prof(:) = getvar1d(cf_tfil, cn_vdeptht, npk     )
+    tim     = getvar1d(cf_tfil, cn_vtimec,  npt     )
+    ierr    = putvar1d(ncout,   tim,        npt, 'T')
+  END SUBROUTINE CreateOutput
+
+  SUBROUTINE ProjectOverIso ( ptab3, ptabint, ptab1d, ptabint2 )
+    !!---------------------------------------------------------------------
+    !!                  ***  ROUTINE ProjectOverIso  ***
+    !!
+    !! ** Purpose :  Project the value of ptab3 (3d) on a isopycnic surface
+    !!
+    !! ** Method  :  We use pre-computed interpolation coefficients 
+    !!
+    !!----------------------------------------------------------------------
+    REAL(KIND=4), DIMENSION (:,:,:),         INTENT (in  ) :: ptab3
+    REAL(KIND=4), DIMENSION (:,:),           INTENT ( out) :: ptabint
+    REAL(KIND=4), DIMENSION (:),   OPTIONAL, INTENT (in  ) :: ptab1d
+    REAL(KIND=4), DIMENSION (:,:), OPTIONAL, INTENT ( out) :: ptabint2
+    !
+    LOGICAL :: ll_1d = .FALSE.
+    LOGICAL :: ll_good
+    !!----------------------------------------------------------------------
+    IF ( PRESENT (ptab1d) ) ll_1d=.TRUE.
+     DO ji=1,npiglo
+        DO jj=1,npjglo
+           ! ik0 is retrieved from alpha, taking the integer part.
+           ! The remnant is alpha. 
+           ik0=INT(alpha(ji,jj))
+           alpha(ji,jj) =  alpha(ji,jj) - ik0
+           IF (ik0 /= 0) THEN
+              P1=ptab3(ji,jj,ik0)
+              P2=ptab3(ji,jj,ik0+1)
+              ll_good = (P1 /= zspval .AND. P2 /= zspval)
+              IF ( ll_good ) THEN
+                 ptabint(ji,jj)  = alpha(ji,jj) * P2          + (1-alpha(ji,jj)) * P1
+              ELSE
+                 ptabint(ji,jj) = zspval
+              ENDIF
+           ELSE
+              ptabint(ji,jj) = zspval
+              zint(ji,jj)    = zspval
+           ENDIF
+           IF ( ll_1d ) THEN
+             IF (ik0 /= 0) THEN
+                P1=ptab1d(ik0)
+                P2=ptab1d(ik0+1)
+                IF ( ll_good ) THEN
+                   ptabint2(ji,jj)  = alpha(ji,jj) * P2       + (1-alpha(ji,jj)) * P1
+                ELSE
+                   ptabint2(ji,jj)  = zspval
+                ENDIF
+             ELSE
+                ptabint2(ji,jj) = zspval
+             ENDIF
+           ENDIF
+           ! re-add ik0 to alpha for the next computation
+           alpha(ji,jj) =  alpha(ji,jj) + ik0
+        END DO
+     END DO
+  END SUBROUTINE ProjectOverIso
+
 
 END PROGRAM cdfisopsi
