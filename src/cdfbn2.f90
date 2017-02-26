52e78d006729c2805896d02c950b7fe4393c5daa
jmm <Jean-Marc.Molines@univ-grenoble-alpes.fr>
2017-02-26 22:38:23 +0100

    add vvl option in cdfbn2
      * add -o, -nc4 options as well
      * add CreateOutput routine

--- src/cdfbn2.f90
+++ src/cdfbn2.f90
@@ -27,14 +27,14 @@ PROGRAM cdfbn2
   IMPLICIT NONE
 
   INTEGER(KIND=4)                              :: jk, jt                   ! dummy loop index
+  INTEGER(KIND=4)                              :: it                       ! time index for vvl
   INTEGER(KIND=4)                              :: ierr                     ! working integer
   INTEGER(KIND=4)                              :: narg, iargc, ijarg       ! 
   INTEGER(KIND=4)                              :: npiglo, npjglo, npk, npt ! size of the domain
   INTEGER(KIND=4)                              :: iup = 1, idown = 2, itmp ! for swapping the levels
   INTEGER(KIND=4)                              :: ncout                    ! ncid of output file
-  INTEGER(KIND=4), DIMENSION(2)                :: ipk, id_varout           ! level and id of output variables
+  INTEGER(KIND=4), DIMENSION(1)                :: ipk, id_varout           ! level and id of output variables
 
-  REAL(KIND=4)                                 :: zpi                      ! 3.14...
   REAL(KIND=4), DIMENSION (:,:,:), ALLOCATABLE :: ztemp, zsal, zwk         ! Array to read 2 layer of data
   REAL(KIND=4), DIMENSION (:,:),   ALLOCATABLE :: zn2                      ! Brunt Vaissala Frequency (N2)
   REAL(KIND=4), DIMENSION (:,:),   ALLOCATABLE :: zmask, e3w               ! mask and metric
@@ -43,38 +43,45 @@ PROGRAM cdfbn2
   CHARACTER(LEN=256)                           :: cf_tfil, cldum, cv_dep   ! input file name, ...
   CHARACTER(LEN=256)                           :: cf_out = 'bn2.nc'        ! output file name
   CHARACTER(LEN=256)                           :: cglobal                  ! global attribute
-  CHARACTER(LEN=80)                            :: cv_e3w  = 'e3w_ps'       ! e3w variable name (partial step)
+  CHARACTER(LEN=80)                            :: cf_e3w                   ! file with e3w in case of vvl
   CHARACTER(LEN=80)                            :: cv_bn2  = 'vobn2'        ! cdf variable name for N2
 
   TYPE(variable), DIMENSION(1)                 :: stypvar                  ! variable attribute
 
-  LOGICAL                                      :: l_w=.false.              ! flag for vertical location of bn2
-  LOGICAL                                      :: lchk=.true.              ! check missing files
-  LOGICAL                                      :: lfull=.false.            ! full step flag
+  LOGICAL                                      :: l_w   =.false.           ! flag for vertical location of bn2
+  LOGICAL                                      :: lchk                     ! check missing files
+  LOGICAL                                      :: lfull =.false.           ! full step flag
+  LOGICAL                                      :: lnc4  =.false.           ! full step flag
   !!----------------------------------------------------------------------
   CALL ReadCdfNames()
 
   narg = iargc()
   IF ( narg == 0 ) THEN
-     PRINT *,' usage : cdfbn2  T-file [W] [-full]'
+     PRINT *,' usage : cdfbn2  -f T-file [-W] [-full] [-o OUT-file] [-nc4] [-vvl]'
      PRINT *,'     PURPOSE :'
      PRINT *,'       Compute the Brunt-Vaissala frequency (N2) according to' 
      PRINT *,'       temperature and salinity given in the input file.'
      PRINT *,'      '
      PRINT *,'     ARGUMENTS :'
-     PRINT *,'       T-file : netcdf input gridT file for temperature and salinity.' 
+     PRINT *,'       -f T-file : netcdf input gridT file for temperature and salinity.' 
      PRINT *,'      '
      PRINT *,'     OPTIONS :'
-     PRINT *,'       [ W ] : keep N2 at W points. Default is to interpolate N2' 
+     PRINT *,'       [-W ] : keep N2 at W points. Default is to interpolate N2' 
      PRINT *,'             at T point on the vertical.'
-     PRINT *,'       [ -full ] : indicate a full step configuration instead of'
+     PRINT *,'       [-full ] : indicate a full step configuration instead of'
      PRINT *,'                the default partial steps.'
+     PRINT *,'       [-o OUT-file ] : specify output file name instead of ',TRIM(cf_out)
+     PRINT *,'       [-nc4 ]  : Use netcdf4 output with chunking and deflation level 1'
+     PRINT *,'                 This option is effective only if cdftools are compiled with'
+     PRINT *,'                 a netcdf library supporting chunking and deflation.'
+     PRINT *,'       [-vvl W-file ] : use time-varying vertical metrics, W-file is a file'
+     PRINT *,'                 holding e3w(t) for vvl.'
      PRINT *,'      '
      PRINT *,'     REQUIRED FILES :'
      PRINT *,'       ',TRIM(cn_fzgr),' is needed for this program.' 
      PRINT *,'      '
      PRINT *,'     OUTPUT : '
-     PRINT *,'       netcdf file : ', TRIM(cf_out) 
+     PRINT *,'       netcdf file : ', TRIM(cf_out) ,' unless -o option specified'
      PRINT *,'         variables : ', TRIM(cv_bn2)
      STOP
   ENDIF
@@ -82,36 +89,32 @@ PROGRAM cdfbn2
   cglobal = 'Partial step computation'
 
   ijarg = 1
-  CALL getarg (ijarg, cf_tfil) ; ijarg = ijarg + 1
-
   DO WHILE ( ijarg <= narg ) 
      CALL getarg(ijarg, cldum) ; ijarg = ijarg + 1
      SELECT CASE (cldum)
-     CASE ('W','w') ; l_w   = .true.
-     CASE ('-full') ; lfull = .true. ; cglobal = 'full step computation'
+     CASE ( '-f'    ) ; CALL getarg(ijarg, cf_tfil) ; ijarg = ijarg + 1
+     CASE ( '-W'    ) ; l_w     = .TRUE.
+     CASE ( '-full' ) ; lfull   = .TRUE. ; cglobal = 'full step computation'
+     CASE ( '-o'    ) ; CALL getarg(ijarg, cf_out ) ; ijarg = ijarg + 1
+     CASE ( '-nc4'  ) ; lnc4    = .TRUE.
+     CASE ( '-vvl'  ) ; lg_vvl  = .TRUE. 
+                        CALL getarg(ijarg, cf_e3w ) ; ijarg = ijarg + 1
      CASE DEFAULT   ; PRINT *,' Option not understood :', TRIM(cldum) ; STOP
      END SELECT
   END DO
 
   lchk = chkfile (cn_fzgr )
   lchk = lchk .OR. chkfile (cf_tfil  )
+  IF (lg_vvl ) lchk = lchk .OR. chkfile (cf_e3w) 
   IF ( lchk  ) STOP  ! missing files 
+
+  IF ( lg_vvl ) cn_fe3w = cf_e3w
 
   npiglo = getdim (cf_tfil, cn_x)
   npjglo = getdim (cf_tfil, cn_y)
   npk    = getdim (cf_tfil, cn_z)
   npt    = getdim (cf_tfil, cn_t)
 
-  ipk(1)                       = npk  !  3D
-  stypvar(1)%cname             = cv_bn2
-  stypvar(1)%cunits            = 's-1'
-  stypvar(1)%rmissing_value    = -1000.
-  stypvar(1)%valid_min         = 0.
-  stypvar(1)%valid_max         = 50000.
-  stypvar(1)%clong_name        = 'Brunt_Vaissala_Frequency'
-  stypvar(1)%cshort_name       = cv_bn2
-  stypvar(1)%conline_operation = 'N/A'
-  stypvar(1)%caxis             = 'TZYX'
 
   PRINT *, 'npiglo =', npiglo
   PRINT *, 'npjglo =', npjglo
@@ -129,20 +132,15 @@ PROGRAM cdfbn2
 
   gdep(:) = getvare3(cn_fzgr, cv_dep, npk)
 
-  ! create output fileset
-  ncout = create      (cf_out,   cf_tfil,  npiglo, npjglo, npk)
-  ierr  = createvar   (ncout ,   stypvar,  1,      ipk,    id_varout, cdglobal=TRIM(cglobal))
-  ierr  = putheadervar(ncout,    cf_tfil,  npiglo, npjglo, npk, pdep=gdep)
-
-  zpi=ACOS(-1.)
-
-  tim  = getvar1d(cf_tfil, cn_vtimec, npt   )
-  ierr = putvar1d(ncout,  tim,       npt,'T')
+  CALL CreateOutput
 
   IF ( lfull )  e3w1d(:) = getvare3(cn_fzgr, cn_ve3w, npk)
 
   gdep(:) = getvare3(cn_fzgr, cn_gdepw, npk)
   DO jt=1,npt
+     IF ( lg_vvl ) THEN ; it=jt
+     ELSE               ; it=1
+     ENDIF
      !  2 levels of T and S are required : iup,idown (with respect to W level)
      !  Compute from bottom to top (for vertical integration)
      ztemp(:,:,idown) = getvar(cf_tfil, cn_votemper,  npk-1, npiglo, npjglo, ktime=jt)
@@ -155,10 +153,8 @@ PROGRAM cdfbn2
         WHERE(ztemp(:,:,idown) == 0 ) zmask = 0
         zsal(:,:,iup) = getvar(cf_tfil, cn_vosaline, jk-1, npiglo, npjglo, ktime=jt)
 
-        IF ( lfull ) THEN
-           e3w(:,:) = e3w1d(jk)
-        ELSE
-           e3w(:,:) = getvar(cn_fzgr, cv_e3w  , jk, npiglo, npjglo, ldiom=.true.)
+        IF ( lfull ) THEN ; e3w(:,:) = e3w1d(jk)
+        ELSE              ; e3w(:,:) = getvar(cn_fe3w, cn_ve3w , jk, npiglo, npjglo, ktime=it, ldiom=.NOT.lg_vvl )
         ENDIF
 
         zwk(:,:,iup) = eosbn2(ztemp, zsal, gdep(jk), e3w, npiglo, npjglo ,iup, idown)* zmask(:,:)
@@ -182,5 +178,38 @@ PROGRAM cdfbn2
   END DO
 
   ierr = closeout(ncout)
+
+CONTAINS
+
+  SUBROUTINE CreateOutput
+    !!---------------------------------------------------------------------
+    !!                  ***  ROUTINE CreateOutput  ***
+    !!
+    !! ** Purpose :  Create netcdf output file(s) 
+    !!
+    !! ** Method  :  Use stypvar global description of variables
+    !!
+    !!----------------------------------------------------------------------
+  ipk(1)                       = npk  !  3D
+  stypvar(1)%ichunk            = (/npiglo,MAX(1,npjglo/30),1,1 /)
+  stypvar(1)%cname             = cv_bn2
+  stypvar(1)%cunits            = 's-1'
+  stypvar(1)%rmissing_value    = -1000.
+  stypvar(1)%valid_min         = 0.
+  stypvar(1)%valid_max         = 50000.
+  stypvar(1)%clong_name        = 'Brunt_Vaissala_Frequency'
+  stypvar(1)%cshort_name       = cv_bn2
+  stypvar(1)%conline_operation = 'N/A'
+  stypvar(1)%caxis             = 'TZYX'
+
+  ! create output fileset
+  ncout = create      (cf_out,   cf_tfil,  npiglo, npjglo, npk,                               ld_nc4=lnc4 )
+  ierr  = createvar   (ncout ,   stypvar,  1,      ipk,    id_varout, cdglobal=TRIM(cglobal), ld_nc4=lnc4 )
+  ierr  = putheadervar(ncout,    cf_tfil,  npiglo, npjglo, npk, pdep=gdep)
+
+  tim  = getvar1d(cf_tfil, cn_vtimec, npt    )
+  ierr = putvar1d(ncout,  tim,        npt,'T')
+
+  END SUBROUTINE CreateOutput
 
 END PROGRAM cdfbn2
