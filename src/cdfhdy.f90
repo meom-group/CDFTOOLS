8ae024e58fb7cb37d31abfce7886ebaf49e066ef
jmm <Jean-Marc.Molines@univ-grenoble-alpes.fr>
2017-03-04 12:39:26 +0100

    Cleaning in cdfhdy.f90
      * think about vvl option in this tool (? )

--- src/cdfhdy.f90
+++ src/cdfhdy.f90
@@ -25,7 +25,7 @@ PROGRAM cdfhdy
 
   INTEGER(KIND=4)                           :: jk, jt           ! dummy loop index
   INTEGER(KIND=4)                           :: ierr             ! working integer
-  INTEGER(KIND=4)                           :: narg, iargc      ! browse arguments
+  INTEGER(KIND=4)                           :: narg, iargc,ijarg! browse arguments
   INTEGER(KIND=4)                           :: npiglo, npjglo   ! size of the domain
   INTEGER(KIND=4)                           :: npk, npt         !  "           "
   INTEGER(KIND=4)                           :: nlev1, nlev2     ! limit of vertical integration
@@ -50,12 +50,14 @@ PROGRAM cdfhdy
   CHARACTER(LEN=256)                        :: cldum            ! dummy string
 
   TYPE(variable) , DIMENSION(1)             :: stypvar          ! structure for attributes
+
+  LOGICAL                                   :: lnc4 = .FALSE.   ! Use nc4 with chunking and deflation
   !!----------------------------------------------------------------------
   CALL ReadCdfNames()
 
   narg= iargc()
   IF ( narg <= 3 ) THEN
-     PRINT *,' usage : cdfhdy T-file level1 level2'
+     PRINT *,' usage : cdfhdy -f T-file -limit level1 level2 [-o OUT-file] [-nc4] '
      PRINT *,'      '
      PRINT *,'     PURPOSE :'
      PRINT *,'        Compute dynamical height anomaly field from gridT file.'
@@ -64,22 +66,39 @@ PROGRAM cdfhdy
      PRINT *,'            10e4 factor is for the conversion decibar to pascal.'
      PRINT *,'      '
      PRINT *,'     ARGUMENTS :'
-     PRINT *,'        T-file  : netcdf file with temperature and salinity'
-     PRINT *,'        level1  : upper limit for vertical integration (usually 1 = surface)'
-     PRINT *,'        level2  : lower limit for vertical integration.'
+     PRINT *,'        -f T-file  : netcdf file with temperature and salinity'
+     PRINT *,'        -limit level1  level2  : level1 is the upper limit for vertical '
+     PRINT *,'                   integration (usually 1 = surface), while level2 is the'
+     PRINT *,'                   lower limit for vertical integration.'
+     PRINT *,'      '
+     PRINT *,'     OPTIONS :'
+     PRINT *,'        [-o OUT-file] : specify name of the output file instead of ', TRIM(cf_out) 
+     PRINT *,'        [-nc4   ]     : Use netcdf4 output with chunking and deflation level 1'
+     PRINT *,'                 This option is effective only if cdftools are compiled with'
+     PRINT *,'                 a netcdf library supporting chunking and deflation.'
      PRINT *,'      '
      PRINT *,'     REQUIRED FILES :'
      PRINT *,'       ',TRIM(cn_fmsk),' and ', TRIM(cn_fzgr)
      PRINT *,'      '
      PRINT *,'     OUTPUT : '
-     PRINT *,'       netcdf file : ', TRIM(cf_out) 
+     PRINT *,'       netcdf file : ', TRIM(cf_out) ,' unless -o option is used.'
      PRINT *,'         variables : sohdy (m)'
      STOP
   ENDIF
 
-  CALL getarg (1, cf_tfil )
-  CALL getarg (2, cldum   ) ;        READ(cldum,*) nlev1
-  CALL getarg (3, cldum   ) ;        READ(cldum,*) nlev2
+  ijarg=1
+  DO WHILE (ijarg <= narg )
+     CALL getarg( ijarg, cldum ) ; ijarg=ijarg+1
+     SELECT CASE ( cldum )
+     CASE ( '-f'     ) ;  CALL getarg( ijarg, cf_tfil) ; ijarg=ijarg+1
+     CASE ( '-limit' ) ;  CALL getarg( ijarg, cldum  ) ; ijarg=ijarg+1  ;  READ(cldum,*) nlev1
+        ;  CALL getarg( ijarg, cldum  ) ; ijarg=ijarg+1  ;  READ(cldum,*) nlev2
+        ! options
+     CASE ( '-o'     ) ;  CALL getarg( ijarg, cf_out ) ; ijarg=ijarg+1
+     CASE ( '-nc4'   ) ;  lnc4 = .TRUE.
+     CASE DEFAULT      ;  PRINT *,' ERROR : ', TRIM(cldum),' : unknown option.' ; STOP
+     END SELECT
+  ENDDO
 
   IF ( chkfile (cf_tfil) .OR. chkfile(cn_fmsk) .OR. chkfile(cn_fzgr)  ) STOP ! missing file
 
@@ -87,18 +106,6 @@ PROGRAM cdfhdy
   npjglo = getdim (cf_tfil,cn_y)
   npk    = getdim (cf_tfil,cn_z)
   npt    = getdim (cf_tfil,cn_t)
-
-  ipk(:) = 1 
-  stypvar(1)%cname= 'sohdy'
-  stypvar(1)%cunits='m'
-  stypvar(1)%rmissing_value=0.
-  stypvar(1)%valid_min= -100.
-  stypvar(1)%valid_max= 100.
-  stypvar(1)%clong_name='Dynamical height anomaly'
-  stypvar(1)%cshort_name='sohdy'
-  stypvar(1)%conline_operation='N/A'
-  stypvar(1)%caxis='TYX'
-
 
   PRINT *, 'npiglo=', npiglo
   PRINT *, 'npjglo=', npjglo
@@ -110,25 +117,19 @@ PROGRAM cdfhdy
   ALLOCATE (rdep(npiglo,npjglo), rdepth(npiglo,npjglo), ssh(npiglo,npjglo), e3t_1d(npk))
   ALLOCATE (tim(npt))
 
-  ! create output fileset
-  ncout = create      (cf_out, cf_tfil,  npiglo, npjglo, npk       )
-  ierr  = createvar   (ncout,  stypvar,  1,      ipk,    id_varout )
-  ierr  = putheadervar(ncout,  cf_tfil,  npiglo, npjglo, npk       )
-
-  tim   = getvar1d(cf_tfil, cn_vtimec, npt     )
-  ierr  = putvar1d(ncout,   tim,       npt, 'T')
+  CALL CreateOutput
 
   ! Temperature and salinity for reference profile
   temp0(:,:) =  0.
-  sal0(:,:)  = 35.
+  sal0 (:,:) = 35.
 
-  tmask(:,:) = getvar(cn_fmsk, 'tmask', nlev2, npiglo, npjglo)
+  tmask(:,:) = getvar(cn_fmsk,  cn_tmask, nlev2, npiglo, npjglo)
   ssh(:,:)   = getvar(cf_tfil,  cn_sossheig, 1,  npiglo, npjglo)
   e3t_1d(:)  = getvare3(cn_fzgr, cn_ve3t, npk)
 
   DO jt=1,npt
      PRINT *,' TIME = ', jt, tim(jt)/86400.,' days'
-     dhdy(:,:)    = 0.
+     dhdy  (:,:) = 0.d0
      rdepth(:,:) = 0.
 
      DO jk = nlev1, nlev2
@@ -144,8 +145,8 @@ PROGRAM cdfhdy
         ! depth at current level, including ssh (used for computation of rho in situ)
         rdepth(:,:) = rdepth(:,:) + rdep(:,:)
 
-        temp(:,:)= getvar(cf_tfil, cn_votemper,  jk ,npiglo, npjglo, ktime=jt)
-        sal(:,:) = getvar(cf_tfil, cn_vosaline,  jk ,npiglo, npjglo, ktime=jt)
+        temp(:,:) = getvar(cf_tfil, cn_votemper,  jk, npiglo, npjglo, ktime=jt)
+        sal (:,:) = getvar(cf_tfil, cn_vosaline,  jk, npiglo, npjglo, ktime=jt)
 
         dsig0 = sigmai(temp0, sal0, rdepth, npiglo, npjglo) 
         dsig  = sigmai(temp , sal , rdepth, npiglo, npjglo) 
@@ -170,5 +171,37 @@ PROGRAM cdfhdy
   END DO  ! next time frame
 
   ierr = closeout(ncout)
+CONTAINS
+
+  SUBROUTINE CreateOutput
+    !!---------------------------------------------------------------------
+    !!                  ***  ROUTINE CreateOutput  ***
+    !!
+    !! ** Purpose :  Create netcdf output file(s) 
+    !!
+    !! ** Method  :  Use stypvar global description of variables
+    !!
+    !!----------------------------------------------------------------------
+    ipk(:) = 1 
+    stypvar(1)%ichunk           = (/npiglo,MAX(1,npjglo/30),1,1 /)
+    stypvar(1)%cname            = 'sohdy'
+    stypvar(1)%cunits           = 'm'
+    stypvar(1)%rmissing_value   = 0.
+    stypvar(1)%valid_min        = -100.
+    stypvar(1)%valid_max        =  100.
+    stypvar(1)%clong_name       = 'Dynamical height anomaly'
+    stypvar(1)%cshort_name      = 'sohdy'
+    stypvar(1)%conline_operation= 'N/A'
+    stypvar(1)%caxis            = 'TYX'
+
+    ! create output fileset
+    ncout = create      (cf_out, cf_tfil,  npiglo, npjglo, npk       , ld_nc4=lnc4 )
+    ierr  = createvar   (ncout,  stypvar,  1,      ipk,    id_varout , ld_nc4=lnc4 )
+    ierr  = putheadervar(ncout,  cf_tfil,  npiglo, npjglo, npk       )
+
+    tim   = getvar1d(cf_tfil, cn_vtimec, npt     )
+    ierr  = putvar1d(ncout,   tim,       npt, 'T')
+
+  END SUBROUTINE CreateOutput
 
 END PROGRAM cdfhdy
