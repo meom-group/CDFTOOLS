110d11246caa2ea848b2bae46e5c2867e62271d8
jmm <Jean-Marc.Molines@univ-grenoble-alpes.fr>
2017-03-13 07:58:37 +0100

    introduce some getenv statement into modcdfnames.f90
     * mesh, mask files names which are by default mesh_hgr.nc, mesh_zgr.nc and mask.nc can be changed by setting the corresponding environnment variables
       - CDFT_MESH_HGR
       - CDFT_MESH_ZFR
       - CDFT_MASK
     * In the same manner I also provide facilies for basins mask ( default new_maskglo.nc), and coordinate file (oordinates.nc).
       - CDFT_BASINS
       - CDFT_COORD

--- src/modcdfnames.f90
+++ src/modcdfnames.f90
@@ -274,6 +274,7 @@ CONTAINS
        cl_namlist=cldum
        INQUIRE( FILE=cl_namlist, EXIST= ll_exist )
        IF ( .NOT. ll_exist ) THEN
+          CALL chkenv
           RETURN    ! assuming that there is no need to read 
                     ! a namelist for cdf names
        ENDIF
@@ -293,6 +294,7 @@ CONTAINS
     READ(inam, namsqdvar  )
     READ(inam, nammeshmask  )
     CLOSE ( inam ) 
+    CALL chkenv
 
   END SUBROUTINE ReadCdfNames
 
@@ -329,5 +331,20 @@ CONTAINS
     CLOSE (iout)
 
   END SUBROUTINE PrintCdfNames
+
+  SUBROUTINE chkenv
+
+  CHARACTER(LEN=255) :: cldum
+! provide facilities for environment mariables fixing basic filename
+    CALL getenv('CDFT_MESH_HGR',cldum ) ; IF ( cldum /= '' ) cn_fhgr    = cldum
+    CALL getenv('CDFT_MESH_ZGR',cldum ) ; IF ( cldum /= '' ) cn_fzgr    = cldum
+    CALL getenv('CDFT_MASK'    ,cldum ) ; IF ( cldum /= '' ) cn_fmsk    = cldum
+    CALL getenv('CDFT_BASINS'  ,cldum ) ; IF ( cldum /= '' ) cn_fbasins = cldum
+    CALL getenv('CDFT_COORD'   ,cldum ) ; IF ( cldum /= '' ) cn_fcoo    = cldum
+
+! .. can be extended
+  END SUBROUTINE chkenv
+
+
 
 END MODULE modCdfNames
