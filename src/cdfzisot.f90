576e6b7ddb0d8ffe1292f55335c43891e96f8f26
jmm <Jean-Marc.Molines@univ-grenoble-alpes.fr>
2017-03-16 00:15:17 +0100

    Normalization of cdfzisot

--- src/cdfzisot.f90
+++ src/cdfzisot.f90
@@ -26,6 +26,7 @@ PROGRAM cdfzisot
   INTEGER(KIND=4)                              :: ji, jj, jk, jt ! dummy loop index
   INTEGER(KIND=4)                              :: jref           ! dummy loop index
   INTEGER(KIND=4)                              :: narg, iargc, ii ! browse line
+  INTEGER(KIND=4)                              :: ijarg          ! line parser
   INTEGER(KIND=4)                              :: npiglo, npjglo ! domain size
   INTEGER(KIND=4)                              :: npk, npt       ! domain size
   INTEGER(KIND=4)                              :: ncout, ierr    ! ncid of output file, error status
@@ -43,88 +44,67 @@ PROGRAM cdfzisot
   REAL(KIND=4), DIMENSION(:,:),    ALLOCATABLE :: glam,gphi      ! lon/lat
   REAL(KIND=4), DIMENSION(:,:),    ALLOCATABLE :: rzisot         ! depth of the isotherm
   REAL(KIND=4), DIMENSION(:,:),    ALLOCATABLE :: rzisotup       ! depth of the isotherm above
-                                                                 ! in case of inversion
+  ! in case of inversion
 
   CHARACTER(LEN=256)                           :: cf_tfil        ! input T file
   CHARACTER(LEN=256)                           :: cf_out='zisot.nc'! defaults output file name
-  CHARACTER(LEN=256)                           :: cdum           ! dummy value
+  CHARACTER(LEN=256)                           :: cldum           ! dummy value
 
   TYPE(variable), DIMENSION(pnvarout)          :: stypvar        ! structure for output var. attributes
+
+  LOGICAL                                      :: lnc4 = .FALSE.  ! Use nc4 with chunking and deflation
   !!----------------------------------------------------------------------
   CALL ReadCdfNames()
 
   narg = iargc()
-  IF ( narg < 2 ) THEN
-     PRINT *,' usage : cdfzisot T-file RefTemp [Output File]'
+  IF ( narg == 0 ) THEN
+     PRINT *,' usage : cdfzisot -t T-file -iso ISO-temp [-o OUT-file] [-nc4]'
      PRINT *,'      '
      PRINT *,'     PURPOSE :'
-     PRINT *,'       Compute depth of an isotherm given as argument'
+     PRINT *,'       Computes the depth of an isotherm surface from the temperature file'
+     PRINT *,'       and value of the isotherm given on the command line.'
      PRINT *,'      '
      PRINT *,'     ARGUMENTS :'
-     PRINT *,'       T-file  : input netcdf file (gridT)' 
-     PRINT *,'       RefTemp : Temperature of the isotherm.'
-     PRINT *,'       Output File : netCDF Optional (defaults: zisot.nc)' 
+     PRINT *,'       -t T-file  : input netcdf file with the ocean temperature.' 
+     PRINT *,'       -iso ISO-temp : Indicates the temperature (Celsius) of the chosen'
+     PRINT *,'            isotherm.'
+     PRINT *,'      '
+     PRINT *,'     OPTIONS :'
+     PRINT *,'        [-o OUT-file] : specify the output file name instead of ',TRIM(cf_out)
+     PRINT *,'        [-nc4]  : Use netcdf4 output with chunking and deflation level 1'
+     PRINT *,'             This option is effective only if cdftools are compiled with'
+     PRINT *,'             a netcdf library supporting chunking and deflation.'
      PRINT *,'      '
      PRINT *,'     REQUIRED FILES :'
      PRINT *,'        ',TRIM(cn_fzgr)
      PRINT *,'         In case of FULL STEP configuration, ',TRIM(cn_fbathylev),' is also required.'
      PRINT *,'      '
      PRINT *,'     OUTPUT : '
-     PRINT *,'       netcdf file : ', TRIM(cf_out) 
+     PRINT *,'       netcdf file : ', TRIM(cf_out) ,' unless option -o is used.'
      STOP
   ENDIF
 
-  ! get input gridT filename
-  CALL getarg (1, cf_tfil)
+  ijarg=1
+  DO WHILE ( ijarg <= narg ) 
+     CALL getarg (ijarg, cldum ) ; ijarg=ijarg+1
+     SELECT CASE ( cldum )
+     CASE ( '-t'  ) ; CALL getarg (ijarg, cf_tfil) ; ijarg=ijarg+1
+     CASE ( '-iso') ; CALL getarg (ijarg, cldum  ) ; ijarg=ijarg+1 ; READ(cldum,*) rtref
+        ! options
+     CASE ( '-o'  ) ; CALL getarg (ijarg, cf_out ) ; ijarg=ijarg+1
+     CASE ( '-nc4') ; lnc4 = .TRUE.
+     CASE DEFAULT   ; PRINT *,' ERROR : ',TRIM(cldum),' : unknown option.' ; STOP 1
+     END SELECT
+  ENDDO
+
   IF ( chkfile(cf_tfil) .OR. chkfile(cn_fzgr) ) STOP ! missing file
-
-  ! get reference temperature
-  CALL getarg (2, cdum)
-  IF (INDEX(cdum,'.') == 0 ) THEN
-     READ(cdum,'(I10)') ii
-     rtref = float(ii)
-  ELSE
-     READ(cdum,'(f12.6)') rtref
-     cdum=cdum( 1:INDEX(cdum,'.')-1 )//'_'//cdum( INDEX(cdum,'.')+1:LEN_TRIM(cdum) )
-  ENDIF
-  IF ( rtref > 50 .OR. rtref < -3. ) THEN
-     PRINT*,'Sea Water temperature on Earth, not Pluton ! ',rtref
-     STOP
-  ENDIF
-
-  ! get (optional) output file name
-  IF ( narg == 3 ) CALL getarg (3, cf_out)
-  PRINT*,TRIM(cf_out)
 
   ! read dimensions 
   npiglo = getdim (cf_tfil,cn_x)
   npjglo = getdim (cf_tfil,cn_y)
   npk    = getdim (cf_tfil,cn_z)
   npt    = getdim (cf_tfil,cn_t)
-!!$  PRINT *, 'npiglo = ', npiglo
-!!$  PRINT *, 'npjglo = ', npjglo
-!!$  PRINT *, 'npk    = ', npk
-!!$  PRINT *, 'npt    = ', npt
 
-
-  ! define structure to write the computed isotherm depth
-  rdep(1) = 0.
-  ipk(:)                    = 1
-  stypvar(1)%cname          = 'zisot'//TRIM(cdum)
-  stypvar(2)%cname          = 'zisotup'//TRIM(cdum)
-  stypvar%cunits            = 'm'
-  stypvar%rmissing_value    = 32767.
-  stypvar%valid_min         = 0.
-  stypvar%valid_max         = 7000.
-  stypvar(1)%clong_name     = 'Depth_of_'//TRIM(cdum)//'C_isotherm' 
-  stypvar(2)%clong_name     = 'Depth_of_'//TRIM(cdum)//'C_upper_isotherm' 
-  stypvar(1)%cshort_name    = 'D'//TRIM(cdum)
-  stypvar(2)%cshort_name    = 'D'//TRIM(cdum)//'up'
-  stypvar%conline_operation = 'N/A'
-  stypvar%caxis             = 'TYX'
-
-
-  ! dynamical allocations
   ALLOCATE (gdept(npk), gdepw(npk), tim(npt) )
   ALLOCATE (rtem(npiglo,npjglo), rtemxz(npiglo,npk) )
   ALLOCATE (tmask(npiglo,npjglo), glam(npiglo,npjglo), gphi(npiglo,npjglo) )
@@ -134,8 +114,9 @@ PROGRAM cdfzisot
   ! read metrics gdept and gdepw
   gdept(:)    = getvare3(cn_fzgr, cn_gdept, npk )
   gdepw(:)    = getvare3(cn_fzgr, cn_gdepw, npk)
+
   ! read "mbathy"
-  mbathy(:,:) = getvar(cn_fzgr,      'mbathy',    1, npiglo, npjglo)
+  mbathy(:,:) = getvar(cn_fzgr, cn_mbathy,    1, npiglo, npjglo)
 
   ! get missing value of votemper
   rmisval = getatt(cf_tfil, cn_votemper, cn_missing_value )
@@ -144,48 +125,31 @@ PROGRAM cdfzisot
   glam(:,:) = getvar(cf_tfil, cn_vlon2d, 1, npiglo, npjglo)
   gphi(:,:) = getvar(cf_tfil, cn_vlat2d, 1, npiglo, npjglo)
 
-
   ! initialize tmask: 1=valid / 0= no valid of SST
   tmask(:,:) = 1.
   rtem( :,:) = getvar(cf_tfil, cn_votemper, 1, npiglo, npjglo, ktime=1 )  
   WHERE ( rtem == rmisval ) tmask = 0.
   ! initialise matrix of results
-  rzisot = 0. ; WHERE ( rtem == rmisval ) rzisot = rmisval
+  rzisot   = 0. ; WHERE ( rtem == rmisval ) rzisot   = rmisval
   rzisotup = 0. ; WHERE ( rtem == rmisval ) rzisotup = rmisval
 
-  ! Create output file, based on existing input gridT file
-  ncout = create      (cf_out, cf_tfil, npiglo, npjglo, 1           )
-  ierr  = createvar   (ncout,  stypvar, pnvarout,      ipk,    id_varout   )
-  ierr  = putheadervar(ncout,  cf_tfil, npiglo, npjglo, 1, pdep=rdep)
+  CALL CreateOutput
 
-  tim  = getvar1d(cf_tfil, cn_vtimec, npt     )
-  ierr = putvar1d(ncout,   tim,       npt, 'T')
-
-
-  ! compute depth for the isotherm, loop first on time
   DO jt=1,npt
-
-     ! then loop on Y axis
      DO jj = 1 , npjglo
-
         ! read temperature on x-z slab
         rtemxz(:,:) = getvarxz(cf_tfil, cn_votemper, jj, npiglo, npk, kimin=1, kkmin=1, ktime=jt )
-
-
-        ! loop on X axis
         DO ji = 1, npiglo
-
-           ! proceed to the depth of isotherm computation if mask = OK
            IF ( tmask(ji,jj) == 1 ) THEN
               IF ( COUNT( rtemxz(ji,:)>=rtref .AND. rtemxz(ji,:) .NE.rmisval ) > 0 ) THEN
 
                  jk = 1 ! count level down
 
                  ! take into account temperature inversion from the surface
-                 IF ( rtemxz(ji,1)<rtref .AND. rtemxz(ji,1) .NE.rmisval ) THEN
+                 IF ( rtemxz(ji,1)<rtref .AND. rtemxz(ji,1) /= rmisval ) THEN
 
                     ! search first level with T >= rtref
-                    DO WHILE ( jk < npk .AND. rtemxz(ji,jk) < rtref .AND. rtemxz(ji,jk) .NE. rmisval )
+                    DO WHILE ( jk < npk .AND. rtemxz(ji,jk) < rtref .AND. rtemxz(ji,jk) /=  rmisval )
                        jref = jk
                        jk = jk + 1
                     ENDDO
@@ -202,7 +166,7 @@ PROGRAM cdfzisot
                  ! then start from the first level with T >= rtref
                  ! and search first value below rtref
                  jref = 0
-                 DO WHILE ( jk < npk .AND. rtemxz(ji,jk) >= rtref .AND. rtemxz(ji,jk) .NE. rmisval )
+                 DO WHILE ( jk < npk .AND. rtemxz(ji,jk) >= rtref .AND. rtemxz(ji,jk) /=  rmisval )
                     jref = jk
                     jk = jk + 1
                  ENDDO
@@ -232,5 +196,45 @@ PROGRAM cdfzisot
   END DO ! time loop
 
   ierr = closeout(ncout)
+
+CONTAINS
+
+  SUBROUTINE CreateOutput
+    !!---------------------------------------------------------------------
+    !!                  ***  ROUTINE CreateOutput  ***
+    !!
+    !! ** Purpose :  Create netcdf output file(s) 
+    !!
+    !! ** Method  :  Use stypvar global description of variables
+    !!
+    !!----------------------------------------------------------------------
+    WRITE(cldum,'(f6.2)') rtref
+    ! define structure to write the computed isotherm depth
+    rdep(1) = 0.
+    ipk(:)                    = 1
+    stypvar(1)%ichunk         = (/npiglo,MAX(1,npjglo/30),1,1 /)
+    stypvar(1)%cname          = 'zisot'//TRIM(cldum)
+    stypvar(2)%ichunk         = (/npiglo,MAX(1,npjglo/30),1,1 /)
+    stypvar(2)%cname          = 'zisotup'//TRIM(cldum)
+    stypvar%cunits            = 'm'
+    stypvar%rmissing_value    = 32767.
+    stypvar%valid_min         = 0.
+    stypvar%valid_max         = 7000.
+    stypvar(1)%clong_name     = 'Depth_of_'//TRIM(cldum)//'C_isotherm' 
+    stypvar(2)%clong_name     = 'Depth_of_'//TRIM(cldum)//'C_upper_isotherm' 
+    stypvar(1)%cshort_name    = 'D'//TRIM(cldum)
+    stypvar(2)%cshort_name    = 'D'//TRIM(cldum)//'up'
+    stypvar%conline_operation = 'N/A'
+    stypvar%caxis             = 'TYX'
+
+    ! Create output file, based on existing input gridT file
+    ncout = create      (cf_out, cf_tfil, npiglo, npjglo, 1               , ld_nc4=lnc4)
+    ierr  = createvar   (ncout,  stypvar, pnvarout,      ipk,    id_varout, ld_nc4=lnc4)
+    ierr  = putheadervar(ncout,  cf_tfil, npiglo, npjglo, 1, pdep=rdep)
+
+    tim  = getvar1d(cf_tfil, cn_vtimec, npt     )
+    ierr = putvar1d(ncout,   tim,       npt, 'T')
+
+  END SUBROUTINE CreateOutput
 
 END PROGRAM cdfzisot
