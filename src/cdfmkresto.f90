a3eb4bc09db9d2e9cc65602fb4e05cea11c5566b
jmm <Jean-Marc.Molines@univ-grenoble-alpes.fr>
2017-05-17 12:36:54 +0200

    introduce cdfmkresto tool (skeleton)

---
+++ src/cdfmkresto.f90
@@ -0,0 +1,23 @@
+PROGRAM cdfmkresto
+  !!======================================================================
+  !!                     ***  PROGRAM  cdfmkresto  ***
+  !!=====================================================================
+  !!  ** Purpose : implement DRAKKAR restoring strategy off-line
+  !!
+  !!  ** Method  : port DRAKKAR tradmp routine off-line
+  !!
+  !! History :  4.0  : 05/2017  : J.M. Molines :  Original code
+  !!----------------------------------------------------------------------
+  !!----------------------------------------------------------------------
+  !!   routines      : description
+  !!----------------------------------------------------------------------
+
+
+  !!----------------------------------------------------------------------
+  !! CDFTOOLS_4.0 , MEOM 2017
+  !! $Id$
+  !! Copyright (c) 2012, J.-M. Molines
+  !! Software governed by the CeCILL licence (Licence/CDFTOOLSCeCILL.txt)
+  !! @class preprocessing
+  !!----------------------------------------------------------------------
+END PROGRAM cdfmkresto
