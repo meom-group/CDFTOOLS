4df81df088083b3381bd612e3203d52d85f612a7
jmm <Jean-Marc.Molines@univ-grenoble-alpes.fr>
2017-07-10 15:33:10 +0200

    Add CMIP6 variable namimg from UKMO fork
     * use key_CMIP6 in order to include modcdfnames_CMIP6.h90 instead of
       the stabdard 'DRAKKAR' names.

--- src/modcdfnames.F90
+++ src/modcdfnames.F90
@@ -11,6 +11,10 @@ MODULE modCdfNames
   IMPLICIT NONE
 
   PUBLIC
+#if defined key_CMIP6
+  INCLUDE 'modcdfnames_CMIP6.h90'
+#else
+  CHARACTER(LEN=256) :: cn_output = 'default'
 
   ! Dimension name : cn_. [ 1 letter only ]
   CHARACTER(LEN=256) :: cn_x='x'               !: longitude, I dimension
@@ -168,6 +172,8 @@ MODULE modCdfNames
   INTEGER(KIND=4) :: nn_cubvar = 2
   CHARACTER(LEN=15), DIMENSION(jp_cubvarmax) :: cn_cubvar = &
       & (/'sossheig','votemper',('        ', ji=3,jp_cubvarmax) /)
+
+#endif
 
 ! INTERFACE  
 !    SUBROUTINE fdate( cldate)
