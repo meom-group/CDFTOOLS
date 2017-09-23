5472c4700d6dbdcd8fce06bed9451c18b93ad70d
nicolas jourdain <nicolas.jourdain@univ-grenoble-alpes.fr>
2017-09-23 15:19:06 +0200

    water column thickness

--- src/cdfthic.f90
+++ src/cdfthic.f90
@@ -33,12 +33,15 @@ PROGRAM cdfthic
   INTEGER(KIND=4)                            :: narg, iargc, ijarg   ! command line 
   INTEGER(KIND=4)                            :: npiglo, npjglo       ! size of the domain
   INTEGER(KIND=4)                            :: npk, npt             ! size of the domain
-  INTEGER(KIND=4)                            :: ncout 
+  INTEGER(KIND=4)                            :: ncout                ! ID for netcdf file
+  INTEGER(KIND=4)                            :: nvout=1              ! number of output variables
   INTEGER(KIND=4), DIMENSION(:), ALLOCATABLE :: ipk, id_varout       ! levels and varid's of output vars
 
   REAL(KIND=4), PARAMETER                    :: ppspval= 9999.99     ! missing value
-  REAL(KIND=4), DIMENSION(:),   ALLOCATABLE  :: e3_1d                 ! vertical metrics in case of full step
-  REAL(KIND=4), DIMENSION(:,:), ALLOCATABLE  :: e3, ssh             ! vertical metric
+  REAL(KIND=4), PARAMETER                    :: eps=1.e-9            ! epsilon
+  REAL(KIND=4), DIMENSION(:),   ALLOCATABLE  :: e3_1d                ! vertical metrics in case of full step
+  REAL(KIND=4), DIMENSION(:,:), ALLOCATABLE  :: e3, ssh              ! vertical metric
+  REAL(KIND=4), DIMENSION(:,:), ALLOCATABLE  :: area1, area2, ztmp   ! used to interpolate ssh
   REAL(KIND=4), DIMENSION(:,:), ALLOCATABLE  :: tmask                ! npiglo x npjglo
 
   REAL(KIND=8), DIMENSION(:),   ALLOCATABLE  :: dtim                 ! time counter
@@ -53,25 +56,22 @@ PROGRAM cdfthic
   LOGICAL                                    :: lchk   =.FALSE.      ! flag for missing files
   LOGICAL                                    :: lssh   =.FALSE.      ! flag for using ssh for top layer thickness
 
-  TYPE(variable), DIMENSION(:), ALLOCATABLE  :: stypvarin            ! stucture for attributes (input)
   TYPE(variable), DIMENSION(:), ALLOCATABLE  :: stypvar              ! extension for attributes
   !!----------------------------------------------------------------------
   CALL ReadCdfNames()
 
   narg= iargc()
   IF ( narg == 0 ) THEN
-     PRINT *,' usage : cdfthic [-vvl <file>] [-T] [-U] [-V]'
-     PRINT *,'                 [-ssh <file>]'
-     PRINT *,'                 [-full]'
-     PRINT *,'                 [-T] [-U] [-V]'
-     PRINT *,'                 [-o OUT-file]'
+     PRINT *,' usage : cdfthic [-vvl <file>] [-ssh <file>] [-full]'
+     PRINT *,'                 [-U] [-V] [-o <OUT-file>]'
      PRINT *,'      '
      PRINT *,'     PURPOSE :'
      PRINT *,'       Compute the water column thickness at T, U, or V points.'
      PRINT *,'      '
      PRINT *,'     OPTIONS :'
-     PRINT *,'        (default)        : partial step computation (constant grid), on T points'
-     PRINT *,'        -full            : full step computation (constant grid)' 
+     PRINT *,'        (default)        : partial step computation (constant grid metrics)'
+     PRINT *,'                           on T points'
+     PRINT *,'        -full            : full step computation (constant grid metrics)' 
      PRINT *,'        -vvl INPUT-file  : directly use time-varying vertical metrics in INPUT-file'
      PRINT *,'        -ssh INPUT-file  : use time-varying ssh and initial grid properties' 
      PRINT *,'        -U               : computation on U points instead of default T points'
@@ -79,11 +79,12 @@ PROGRAM cdfthic
      PRINT *,'        -o OUT-file      : use specified output file instead of ', TRIM(cf_out)
      PRINT *,'      '
      PRINT *,'     REQUIRED FILES :'
-     PRINT *,'      ', TRIM(cn_fzgr),', and ',TRIM(cn_fmsk)
+     PRINT *,'      ', TRIM(cn_fzgr),', and ',TRIM(cn_fhgr),', and ',TRIM(cn_fmsk)
      PRINT *,'      '
      PRINT *,'     OUTPUT : '
      PRINT *,'       netcdf file :  thic.nc (or specified with -o option)'
      PRINT *,'         variables :  thic_T, thic_U, or thic_V (according to grid)'
+     PRINT *,'                      ssh_U or ssh_V (if -ssh option with U or V grids)'
      PRINT *,'      '
      STOP 
   ENDIF
@@ -107,7 +108,7 @@ PROGRAM cdfthic
   IF ( lg_vvl ) ijarg = ijarg + 1
   IF ( lssh   ) ijarg = ijarg + 1
   IF ( lfull  ) ijarg = ijarg + 1
-  if ( ijarg .gt. 1 ) PRINT *,' ERROR : choose no more than one option among -vvl -ssh -full' ; STOP 99
+  IF ( ijarg .GT. 1 ) PRINT *,' ERROR : choose no more than one option among -vvl -ssh -full' ; STOP 99
 
   IF ( lg_vvl ) THEN
     cn_fe3 = cf_in
@@ -127,8 +128,12 @@ PROGRAM cdfthic
   ! Security check
   lchk = chkfile ( cn_fe3  )
   lchk = chkfile ( cn_fzgr ) .OR. lchk
+  lchk = chkfile ( cn_fhgr ) .OR. lchk
   lchk = chkfile ( cn_fmsk ) .OR. lchk
   IF ( lchk ) STOP 99 ! missing files
+
+  IF ( lssh .AND. cgrid .NE. 'T' ) nvout=2
+  ALLOCATE (stypvar(nvout), ipk(nvout), id_varout(nvout))
 
   ! log information so far
   PRINT *,' OUTPUT FILE     : ' , TRIM(cf_out)
@@ -144,15 +149,37 @@ PROGRAM cdfthic
   PRINT *, ' NPT    = ', npt
 
   ! Allocate arrays
-  ALLOCATE (     dtim( npt                    ) )
-  ALLOCATE (    tmask(          npiglo,npjglo ) )
-  ALLOCATE (       e3(          npiglo,npjglo ) )
-  ALLOCATE ( dl_vint1(          npiglo,npjglo ) )
-  IF ( lssh  ) ALLOCATE ( ssh(  npiglo,npjglo ) )
+  ALLOCATE (     dtim( npt                  ) )
+  ALLOCATE (    tmask(        npiglo,npjglo ) )
+  ALLOCATE (       e3(        npiglo,npjglo ) )
+  ALLOCATE ( dl_vint1(        npiglo,npjglo ) )
+  IF ( lssh .AND. cgrid .NE. 'T' ) THEN
+    ALLOCATE (  area1(        npiglo,npjglo ) )
+    ALLOCATE (  area2(        npiglo,npjglo ) )
+    ALLOCATE (   ztmp(        npiglo,npjglo ) )
+  ENDIF
+  IF ( lssh  ) ALLOCATE ( ssh(npiglo,npjglo ) )
   IF ( lfull ) ALLOCATE ( e3_1d( npk ) )
 
   ! Read vertical axis in full-step-case
   IF ( lfull ) e3_1d(:) = getvare3(cn_fzgr, cn_ve3t1d, npk)
+
+  ! Read area fields to interpolate ssh onto U and V grids
+  IF ( lssh .AND. cgrid .NE. 'T' ) THEN
+    area1(:,:) = getvar(cn_fhgr, cn_ve1t, 1, npiglo, npjglo)  ! Save memory: e1t is first read into area
+    ztmp( :,:) = getvar(cn_fhgr, cn_ve2t, 1, npiglo, npjglo)  ! ..and ztmp is used to read e2t
+    area1(:,:) = area1(:,:) * ztmp(:,:)                       ! ..
+    SELECT CASE (cgrid)
+    CASE ('U')
+      area2(:,:) = getvar(cn_fhgr, cn_ve1u, 1, npiglo, npjglo)  ! Save memory: e1u is first read into area
+      ztmp( :,:) = getvar(cn_fhgr, cn_ve2u, 1, npiglo, npjglo)  ! ..and ztmp is used to read e2u
+      area2(:,:) = area2(:,:) * ztmp(:,:)                       ! ..
+    CASE ('V')
+      area2(:,:) = getvar(cn_fhgr, cn_ve1v, 1, npiglo, npjglo)  ! Save memory: e1v is first read into area
+      ztmp( :,:) = getvar(cn_fhgr, cn_ve2v, 1, npiglo, npjglo)  ! ..and ztmp is used to read e2v
+      area2(:,:) = area2(:,:) * ztmp(:,:)                       ! ..
+    END SELECT    
+  ENDIF
 
   CALL CreateOutput
   PRINT *, 'Output file initialised ...'
@@ -182,18 +209,23 @@ PROGRAM cdfthic
        SELECT CASE (cgrid)
          CASE ('T')
            ssh(:,:) = e3(:,:)
-         CASE ('U')
+         CASE ('U') ! ssh at U points
            ssh(npiglo,:) = e3(npiglo,:) ! to improve if periodic domain
-           ssh(1:npiglo-1,:) = 0.5 * ( e3(1:npiglo-1,:) + e3(2:npiglo,:) )  ! ssh at U points
-         CASE ('V')
+           ssh(1:npiglo-1,:) = 0.5 * (  e3(1:npiglo-1,:) * area1(1:npiglo-1,:) &
+           &                          + e3(2:npiglo  ,:) * area1(2:npiglo  ,:) ) / ( eps + area2(1:npiglo-1,:) )
+         CASE ('V') ! ssh at V points
            ssh(:,npjglo) = e3(:,npjglo)
-           ssh(:,1:npjglo-1) = 0.5 * ( e3(:,1:npjglo-1) + e3(:,2:npjglo) )  ! ssh at V points
+           ssh(:,1:npjglo-1) = 0.5 * (  e3(:,1:npjglo-1) * area1(:,1:npjglo-1) &
+           &                          + e3(:,2:npjglo  ) * area1(:,2:npjglo  ) ) / ( eps + area2(:,1:npjglo-1) )
        END SELECT
        dl_vint1(:,:) = dl_vint1(:,:) + ssh(:,:) * 1.d0
      ENDIF
 
      ! Output to netcdf file 
-     ierr = putvar(ncout, id_varout(jvar), REAL(dl_vint1), jk, 1, 1, ktime=jt)
+     ierr = putvar(ncout, id_varout(1), REAL(dl_vint1), jk, 1, 1, ktime=jt)
+     IF ( lssh .AND. cgrid .NE. 'T' ) THEN
+       ierr = putvar(ncout, id_varout(2), ssh, jk, 1, 1, ktime=jt)
+     ENDIF
 
   END DO  ! loop on time
 
@@ -223,6 +255,18 @@ CONTAINS
     stypvar(1)%cshort_name       = 'thickness'
     stypvar(1)%conline_operation = 'N/A'
     stypvar(1)%caxis             = 'TYX'
+
+    IF ( lssh .AND. cgrid .NE. 'T' ) THEN
+      stypvar(2)%cname             = 'ssh_'//TRIM(cgrid)
+      stypvar(2)%cunits            = 'm'
+      stypvar(2)%rmissing_value    = ppspval
+      stypvar(2)%valid_min         = 0.0
+      stypvar(2)%valid_max         = 100.0
+      stypvar(2)%clong_name        = 'Sea Surface Height'
+      stypvar(2)%cshort_name       = 'ssh'
+      stypvar(2)%conline_operation = 'N/A'
+      stypvar(2)%caxis             = 'TYX'
+    ENDIF
 
     ! Initialize output file
     ncout = create      (cf_out, 'none', npiglo, npjglo, 1  )
