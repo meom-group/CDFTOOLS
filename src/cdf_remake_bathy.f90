0207b09f7bd3df72862834296cfb0d06b9fdc21f
jmm <Jean-Marc.Molines@univ-grenoble-alpes.fr>
2021-06-23 16:44:14 +0200

    fix some problem in cdf_remake_bathy.f90
       * still some problem in the computation of hu_0 :
         at U point, thickness of the layer is e3u which is the same as e3t, except at the bottom most cell
         where it is the min(e3t(ji+1,jj), e3t(ji;jj) )
       * same comment for v points...

--- src/cdf_remake_bathy.f90
+++ src/cdf_remake_bathy.f90
@@ -2,7 +2,7 @@ PROGRAM cdf_remake_bathy
   !!======================================================================
   !!                     ***  PROGRAM  cdf_remake_bathy  ***
   !!=====================================================================
-  !!  ** Purpose : Recompute bathymetry from e3t vertical intrgration
+  !!  ** Purpose : Recompute bathymetry from e3t vertical intregration
   !!
   !! History : 4.0  : 09/2019  : Q. Jamet
   !!----------------------------------------------------------------------
@@ -13,26 +13,25 @@ PROGRAM cdf_remake_bathy
   !! $Id$
   !! Copyright (c) 2017, J.-M. Molines 
   !! Software governed by the CeCILL licence (Licence/CDFTOOLSCeCILL.txt)
-  !! @class Equation_of_state
+  !! @class domain_file
   !!----------------------------------------------------------------------
   IMPLICIT NONE
 
 
   INTEGER(KIND=4), PARAMETER                   :: wp=4
-  INTEGER(KIND=4), PARAMETER                   :: pnvarout = 3             ! number of output variables
+  INTEGER(KIND=4), PARAMETER                   :: jpvarout = 3             ! number of output variables
   INTEGER(KIND=4)                              :: ncout                    ! ncid of output file
-  INTEGER(KIND=4), DIMENSION(pnvarout)         :: id_varout                ! id of output variables
-  INTEGER(KIND=4), DIMENSION(pnvarout)         :: ipk                      ! level of output variables
+  INTEGER(KIND=4), DIMENSION(jpvarout)         :: id_varout                ! id of output variables
+  INTEGER(KIND=4), DIMENSION(jpvarout)         :: ipk                      ! level of output variables
   INTEGER(KIND=4)                              :: ierr                     ! working integer
   INTEGER(KIND=4)                              :: narg, iargc, ijarg       !
   INTEGER(KIND=4)                              :: ji, jj, jk, jt           ! dummy loop indices
   INTEGER(KIND=4)                              :: npi, npj, npk, npt       ! size of the domain
- 
-  REAL(wp), DIMENSION(:)      , ALLOCATABLE    :: dtim                     ! time 
+
   REAL(wp), DIMENSION(:,:)    , ALLOCATABLE    :: e3t_0, e3u_0, e3v_0      ! vet. metrics at rest (without vvl)
   REAL(wp), DIMENSION(:,:)    , ALLOCATABLE    :: ht_0, hu_0, hv_0         ! Reference ocean depth at T-, U-, V-points
   REAL(wp), DIMENSION(:,:)    , ALLOCATABLE    :: tmask, umask, vmask      ! Mask at T-, U-, V-points
-  REAL(wp), DIMENSION(:,:)    , ALLOCATABLE    :: nav_lon, nav_lat         ! t-grid horizontal
+  REAL(wp), DIMENSION(:,:)    , ALLOCATABLE    :: rlon, rlat         ! t-grid horizontal
 
   CHARACTER(LEN=255)                           :: cf_mz                    ! vert. mesh  netcdf file name
   CHARACTER(LEN=255)                           :: cf_mask                  ! mask        netcdf file name
@@ -40,33 +39,34 @@ PROGRAM cdf_remake_bathy
   CHARACTER(LEN=256)                           :: cldum                    ! dummy character variable
   CHARACTER(LEN=256)                           :: cglobal                  ! global attribute
 
-  TYPE (variable), DIMENSION(pnvarout)         :: stypvar                  ! structure for attibutes
+  TYPE (variable), DIMENSION(jpvarout)         :: stypvar                  ! structure for attibutes
 
   LOGICAL                                      :: lchk                     ! check missing files
-  LOGICAL                                      :: lfull =.FALSE.           ! full step flag
   LOGICAL                                      :: lnc4  =.FALSE.           ! full step flag
   !!----------------------------------------------------------------------
   CALL ReadCdfNames()
 
   narg= iargc()
   IF ( narg == 0 ) THEN
-     PRINT *,' usage : cdf_remake_bathy -mz ZGR-file -msk MASK-file -o OUT-file'
+     PRINT *,' usage : cdf_remake_bathy -mz ZGR-file -msk MASK-file [-o OUT-file] [-nc4]'
      PRINT *,'      '
      PRINT *,'     PURPOSE :'
-     PRINT *,'      Remake bathymetry from vertical integration of e3t.'
+     PRINT *,'      Remake bathymetry from vertical integration of e3t, read in mesh_zgr.'
      PRINT *,'      '
      PRINT *,'     ARGUMENTS :'
-     PRINT *,'       -mh HGR-file       : netcdf file for vertical mesh'
-     PRINT *,'       -mz ZGR-file       : netcdf file for vertical mesh'
-     PRINT *,'       -mask MASK-file    : netcdf file for mask'
-     PRINT *,'       -o OUT-file        : netcdf output file'
+     PRINT *,'       -mz ZGR-file   : netcdf file for vertical mesh'
+     PRINT *,'       -msk MASK-file : netcdf file for mask'
      PRINT *,'      '
      PRINT *,'     OPTIONS :'
+     PRINT *,'       -o OUT-file    : netcdf output file'
+     PRINT *,'       -nc4           : Use netcdf4/HDF5 with chunking and deflation for output'
      PRINT *,'      '
      PRINT *,'     OUTPUT : '
-     PRINT *,'       netcdf file : this is the netcdf file that will be outputed'
+     PRINT *,'       netcdf file : '//TRIM(cf_out)//' unless -o option is used.'
+     PRINT *,'       variables   : gdepw_0, hu_0, hv_0 '
      PRINT *,'      '
      PRINT *,'     SEE ALSO :'
+     PRINT *,'      cdf_domain2bathy (can probably be merged)'
      PRINT *,'      '
      STOP
   ENDIF
@@ -78,10 +78,9 @@ PROGRAM cdf_remake_bathy
   DO WHILE ( ijarg <= narg ) 
      CALL getarg(ijarg, cldum) ; ijarg = ijarg + 1
      SELECT CASE (cldum)
-     CASE ('-mz'       ) ; CALL getarg( ijarg, cf_mz    ) ; ijarg=ijarg+1
-     CASE ('-msk'      ) ; CALL getarg( ijarg, cf_mask  ) ; ijarg=ijarg+1
+     CASE ('-mz'    ) ; CALL getarg( ijarg, cf_mz    ) ; ijarg=ijarg+1
+     CASE ('-msk'   ) ; CALL getarg( ijarg, cf_mask  ) ; ijarg=ijarg+1
         ! options
-     CASE ( '-full' ) ; lfull   = .TRUE. ; cglobal = 'full step computation'
      CASE ( '-o'    ) ; CALL getarg(ijarg, cf_out ) ; ijarg = ijarg + 1
      CASE ( '-nc4'  ) ; lnc4    = .TRUE.
      CASE DEFAULT     ; PRINT *,' ERROR : ', TRIM(cldum),' : unknown option.' ; STOP 99
@@ -106,6 +105,7 @@ PROGRAM cdf_remake_bathy
   ALLOCATE( e3t_0(npi, npj), e3u_0(npi, npj), e3v_0(npi, npj) )
   ALLOCATE( ht_0(npi, npj) , hu_0(npi, npj) , hv_0(npi, npj)  )
   ALLOCATE( tmask(npi, npj), umask(npi, npj), vmask(npi, npj) )
+  ALLOCATE( rlon(npi, npj) , rlat(npi, npj)                   )
 
   !-- Creat output netcdf files to fill in --
   CALL CreateOutput
@@ -128,7 +128,7 @@ PROGRAM cdf_remake_bathy
      vmask(:,:)           = getvar(cf_mask, 'vmask'  , jk, npi, npj ) 
      hv_0(:,:)            = hv_0(:,:) + e3v_0(:,:) * vmask(:,:)
   END DO
-  
+
   ierr = putvar(ncout, id_varout(1), ht_0, 1, npi, npj , 1 )
   ierr = putvar(ncout, id_varout(2), hu_0, 1, npi, npj , 1 )
   ierr = putvar(ncout, id_varout(3), hv_0, 1, npi, npj , 1 )
@@ -137,7 +137,7 @@ PROGRAM cdf_remake_bathy
 
 CONTAINS
 
-   SUBROUTINE CreateOutput
+  SUBROUTINE CreateOutput
     !!---------------------------------------------------------------------
     !!                  ***  ROUTINE CreateOutput  ***
     !!
@@ -146,6 +146,7 @@ CONTAINS
     !! ** Method  :  Use stypvar global description of variables
     !!
     !!----------------------------------------------------------------------
+    REAL(KIND=8), DIMENSION(npt)        :: dltim                     ! time 
     ! define new variables for output
     ipk(:)                       = 1 
     !stypvar(1)%ichunk            = (/npi,MAX(1,npj/30),1,1 /)
@@ -153,7 +154,7 @@ CONTAINS
     stypvar(1)%cunits            = 'm'
     stypvar(1)%rmissing_value    = 0.
     stypvar(1)%valid_min         = 0.
-    stypvar(1)%valid_max         = 4000.
+    stypvar(1)%valid_max         = 9000.
     stypvar(1)%clong_name        = 'Bathymetry as vert. integ.of e3t_0'
     stypvar(1)%cshort_name       = 'Bathymetry'
     stypvar(1)%conline_operation = 'On T-grid'
@@ -164,7 +165,7 @@ CONTAINS
     stypvar(2)%cunits            = 'm'
     stypvar(2)%rmissing_value    = 0.
     stypvar(2)%valid_min         = 0.
-    stypvar(2)%valid_max         = 4000.
+    stypvar(2)%valid_max         = 9000.
     stypvar(2)%clong_name        = 'Bathymetry as vert. integ.of e3u_0'
     stypvar(2)%cshort_name       = 'Bathymetry'
     stypvar(2)%conline_operation = 'On U-grid'
@@ -175,7 +176,7 @@ CONTAINS
     stypvar(3)%cunits            = 'm'
     stypvar(3)%rmissing_value    = 0.
     stypvar(3)%valid_min         = 0.
-    stypvar(3)%valid_max         = 4000.
+    stypvar(3)%valid_max         = 9000.
     stypvar(3)%clong_name        = 'Bathymetry as vert. integ.of e3v_0'
     stypvar(3)%cshort_name       = 'Bathymetry'
     stypvar(3)%conline_operation = 'On V-grid'
@@ -183,14 +184,20 @@ CONTAINS
 
     ! create output fileset
     ncout   = create      (cf_out  , cf_mz ,  npi, npj, 1 , ld_nc4=lnc4   )
-    ierr    = createvar   (ncout , stypvar ,  pnvarout, ipk , id_varout           , ld_nc4=lnc4 )
-    ierr    = putheadervar(ncout , cf_mz   ,  npi, npj, 1, nav_lon, nav_lat                   )
+    ierr    = createvar   (ncout , stypvar ,  jpvarout, ipk , id_varout   , ld_nc4=lnc4 )
+! JMM remark : rlon and rlat are not initialize....
+!      if variables nav_lon, nav_lat exist in cf_mz, putheaderpar copy them by defaults,
+!      if rlon, rlat not given as arguments... 
+!      The question is to know if those variables exists in the input file...
+!      If yes, then just eliminate references to rlon, rlat (declaration + allocation )
+!   ierr    = putheadervar(ncout , cf_mz   ,  npi, npj, 1, rlon, rlat                   )
+    ierr    = putheadervar(ncout , cf_mz   ,  npi, npj, 1              )
 
-    dtim = getvar1d(cf_mz, cn_vtimec,   npt     )
-    ierr = putvar1d(ncout  , dtim,        npt, 'T')
-    ierr = putvar1d(ncout  , dtim,        npt, 'T')
+    dltim = getvar1d(cf_mz, cn_vtimec,      npt     )
+    ierr  = putvar1d(ncout  , dltim,        npt, 'T')
+    ierr  = putvar1d(ncout  , dltim,        npt, 'T')
 
 
-   END SUBROUTINE CreateOutput
+  END SUBROUTINE CreateOutput
 
-END PROGRAM
+END PROGRAM cdf_remake_bathy
